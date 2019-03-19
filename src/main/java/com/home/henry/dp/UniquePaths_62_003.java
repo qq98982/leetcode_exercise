5abbbd48ff93fafed1b16d3e593c8d3013cfabd4
Henry <hongyu_98982@163.com>
2019-03-19 20:25:11 +0800

    62. Unique Paths

---
+++ src/main/java/com/home/henry/dp/UniquePaths_62_003.java
@@ -0,0 +1,21 @@
+package com.home.henry.dp;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * How many possible unique paths are there?
+ */
+public class UniquePaths_62_003 {
+
+    public int uniquePaths(int m, int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                res[j] = res[j] + res[j - 1];
+            }
+        }
+        return res[n - 1];
+    }
+
+}
