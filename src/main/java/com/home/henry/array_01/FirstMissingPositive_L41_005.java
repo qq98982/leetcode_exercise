6aaca79141cf33e080766556670a2fa929602bfc
Henry <hongyu_98982@163.com>
2019-03-19 08:58:25 +0800

    41. First Missing Positive

---
+++ src/main/java/com/home/henry/array_01/FirstMissingPositive_L41_005.java
@@ -0,0 +1,27 @@
+package com.home.henry.array_01;
+
+/**
+ * Given an unsorted integer array, find the smallest missing positive integer.
+ * Input: [1,2,0] Output: 3    Input: [3,4,-1,1] Output: 2
+ */
+public class FirstMissingPositive_L41_005 {
+
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                int tmp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
