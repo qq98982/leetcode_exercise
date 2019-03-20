84d5a63d0798b613f09a32373297249099198f4b
Henry <hongyu_98982@163.com>
2019-03-20 18:44:59 +0800

    174. Dungeon Game

---
+++ src/main/java/com/home/henry/dp/DungeonGame_L_174_007.java
@@ -0,0 +1,34 @@
+package com.home.henry.dp;
+
+/**
+ * The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
+ * The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
+ * Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).
+ * In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+ */
+public class DungeonGame_L_174_007 {
+
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) { return 0; }
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] = Math.max(dp[i + 1][n - 1] - dungeon[i][n - 1], 1);
+        }
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m - 1][j] = Math.max(dp[m - 1][j + 1] - dungeon[m - 1][j], 1);
+        }
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int down = Math.max(dp[i + 1][j] - dungeon[i][j], 1);
+                int right = Math.max(dp[i][j + 1] - dungeon[i][j], 1);
+                dp[i][j] = Math.min(down, right);
+            }
+        }
+
+        return dp[0][0];
+    }
+
+}
