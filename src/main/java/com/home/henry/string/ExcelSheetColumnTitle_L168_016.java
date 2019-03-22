02d185005a086a1d612b25f00733777c2765ffc5
Henry <hongyu_98982@163.com>
2019-03-22 16:14:51 +0800

    168. Excel Sheet Column Title

---
+++ src/main/java/com/home/henry/string/ExcelSheetColumnTitle_L168_016.java
@@ -0,0 +1,22 @@
+package com.home.henry.string;
+
+/**
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ * 要点: 记得-1
+ */
+public class ExcelSheetColumnTitle_L168_016 {
+
+    public String convertToTitle(int n) {
+        if(n == 0) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            n--;
+            sb.append((char)('A' + n % 26));
+            n = n / 26;
+        }
+        return sb.reverse().toString();
+    }
+
+}
