c51d2816c684b8ca530d651a24197d0818f6177d
sven <hozhu@tibco-support.com>
2019-03-26 21:53:17 +0800

    Minimum windows substring second method

--- src/main/java/com/home/henry/string/MinimumWindowSubstring_L76_022.java
+++ src/main/java/com/home/henry/string/MinimumWindowSubstring_L76_022.java
@@ -1,5 +1,12 @@
 package com.home.henry.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * First is quick, second slower, but there is template for this type questions
+ * Ref: https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/92007/sliding-window-algorithm-template-to-solve-all-the-leetcode-substring-search-problem
+ */
 public class MinimumWindowSubstring_L76_022 {
 
     public String minWindow(String s, String t) {
@@ -23,4 +30,47 @@ public class MinimumWindowSubstring_L76_022 {
 
         return (min == Integer.MAX_VALUE) ? "" : s.substring(from, from + min);
     }
+
+    public String minWindow2(String s, String t) {
+        if (s.length() < t.length()) {
+            return "";
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int count = map.size();
+        int begin = 0, end = 0, head = 0;
+        int len = Integer.MAX_VALUE;
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0) {
+                    count--;
+                }
+            }
+            end++;
+            while (count == 0) {
+                char temp = s.charAt(begin);
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                    if (map.get(temp) > 0) {
+                        count++;
+                    }
+                }
+
+                if (end - begin < len) {
+                    len = end - begin;
+                    head = begin;
+                }
+                begin++;
+            }
+        }
+        if (len == Integer.MAX_VALUE) {
+            return "";
+        }
+        return s.substring(head, head + len);
+    }
+
 }
