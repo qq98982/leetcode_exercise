23b932cb1f7a64bf33fda643fd511b62dd548810
Henry <hongyu_98982@163.com>
2019-03-28 16:07:44 +0800

    104. Maximum Depth of Binary Tree

---
+++ src/main/java/com/home/henry/tree/MaximumDepthBinaryTree_L104_013.java
@@ -0,0 +1,12 @@
+package com.home.henry.tree;
+
+public class MaximumDepthBinaryTree_L104_013 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
