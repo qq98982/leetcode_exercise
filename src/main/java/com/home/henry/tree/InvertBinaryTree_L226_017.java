7c8c28828ceefbc32769daf80a0c66bd97afa0f2
Henry <hongyu_98982@163.com>
2019-03-28 18:01:21 +0800

    226. Invert Binary Tree

---
+++ src/main/java/com/home/henry/tree/InvertBinaryTree_L226_017.java
@@ -0,0 +1,14 @@
+package com.home.henry.tree;
+
+public class InvertBinaryTree_L226_017 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) { return root;}
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+}
