ae3da6dab1bc25afb4cb4f7335bc77a6cd7bebd3
Henry <hongyu_98982@163.com>
2019-04-03 21:24:41 +0800

    69. Sqrt(x)

---
+++ src/main/java/com/home/henry/binary_search_05/Sqrx_L069_005.java
@@ -0,0 +1,30 @@
+package com.home.henry.binary_search_05;
+
+/**
+ * 69. Sqrt(x)
+ * 注意判定条件 mid + 1 > x / mid + 1
+ * 在left向右前, 有可能mid + 1 已经可以返回了
+ */
+class Sqrx_L069_005 {
+
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+        int left = 0, right = x;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (mid > x / mid) {
+                right = mid;
+            } else {
+                if ((mid + 1) > x / (mid + 1)) {
+                    return mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+}
