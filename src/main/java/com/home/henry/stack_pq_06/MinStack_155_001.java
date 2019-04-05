d8e0c992c1421515d787e15bf86df8644a2b5337
Henry <qq98982@gmail.com>
2019-04-06 00:36:58 +0800

    155. Min Stack

---
+++ src/main/java/com/home/henry/stack_pq_06/MinStack_155_001.java
@@ -0,0 +1,36 @@
+package com.home.henry.stack_pq_06;
+
+import java.util.Stack;
+
+/**
+ * 155. Min Stack
+ */
+public class MinStack_155_001 {
+    private Stack<Integer> stack;
+    private int min;
+
+    MinStack_155_001(){
+        this.stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return min;
+    }
+
+}
