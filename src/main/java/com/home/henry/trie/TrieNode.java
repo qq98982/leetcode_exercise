08f4bb4cb888d0384880a4efbd7857f6fc5857f1
Henry <hongyu_98982@163.com>
2019-04-09 17:37:33 +0800

    208. Implement Trie (Prefix Tree)

---
+++ src/main/java/com/home/henry/trie/TrieNode.java
@@ -0,0 +1,14 @@
+package com.home.henry.trie;
+
+public class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+
+    public TrieNode() {}
+
+    TrieNode(char c) {
+        TrieNode node = new TrieNode();
+        node.val = c;
+    }
+}
