51b7d48f61796ad4f64e5c6b92484c1fa552a185
Henry <hongyu_98982@163.com>
2019-04-25 20:35:48 +0800

    64. Minimum Path Sum

--- src/main/java/com/home/henry/dp/MinimumPathSum_L64_005.java
+++ src/main/java/com/home/henry/dp/MinimumPathSum_L64_005.java
@@ -23,4 +23,49 @@ public class MinimumPathSum_L64_005 {
         }
         return grid[grid.length - 1][grid[0].length - 1];
     }
+
+    /**
+     * 滚动数组
+     */
+    public int minPathSumRollingArray(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] f = new int[2][n];
+        int old = 1, now = 0;
+        int i, j , t1, t2;
+        for (i = 0; i < m; i++) {
+            // old is row i-1
+            old = now;
+            // now is row i
+            now = 1 -now;
+            for (j =0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    f[now][j] = grid[i][j];
+                    continue;
+                }
+                f[now][j] = grid[i][j];
+                // from top
+                if (i > 0) {
+                    t1 = f[old][j];
+                } else {
+                    t1 = Integer.MAX_VALUE;
+                }
+                // from left
+                if (j > 0) {
+                    t2 = f[now][j - 1];
+                } else {
+                    t2 = Integer.MAX_VALUE;
+                }
+                if (t1 < t2) {
+                    f[now][j] += t1;
+                } else {
+                    f[now][j] += t2;
+                }
+            }
+        }
+        return f[now][n - 1];
+    }
 }
