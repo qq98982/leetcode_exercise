1eeaad48936d45af739154c8ad8d9b4aec2559cd
Henry <hongyu_98982@163.com>
2019-04-26 11:53:54 +0800

    216. Combination Sum III

---
+++ src/main/java/com/home/henry/back_tracking/CombinationSumIII_L216_006.java
@@ -0,0 +1,31 @@
+package com.home.henry.back_tracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 216. Combination Sum III
+ */
+public class CombinationSumIII_L216_006 {
+
+    static class Solution {
+        public List<List<Integer>> combinationSum3(int k, int n) {
+            List<List<Integer>> res = new ArrayList<>();
+            helper(res, new ArrayList<>(), k, n, 1);
+            return res;
+        }
+
+        private void helper(List<List<Integer>> res, List<Integer> list, int k, int n, int start) {
+            if (k < 0 || n < 0) {return;}
+            if (k == 0 && n == 0) {
+                res.add(new ArrayList(list));
+            } else {
+                for (int i = start; i <= 9; i++) {
+                    list.add(i);
+                    helper(res, list, k - 1, n - i, i + 1);
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+    }
+}
