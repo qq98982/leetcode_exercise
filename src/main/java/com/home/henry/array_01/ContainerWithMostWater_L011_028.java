536455a669a4aff1b11d7093a6c6d47e280c1bd4
Henry <hongyu_98982@163.com>
2019-06-28 15:48:29 +0800

    11. Container With Most Water

---
+++ src/main/java/com/home/henry/array_01/ContainerWithMostWater_L011_028.java
@@ -0,0 +1,19 @@
+package com.home.henry.array_01;
+
+public class ContainerWithMostWater_L011_028 {
+    class Solution {
+        public int maxArea(int[] height) {
+            int res = 0;
+            int l = 0, r = height.length - 1;
+            while (l < r) {
+                res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+                if (height[l] < height[r]) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+            return res;
+        }
+    }
+}
