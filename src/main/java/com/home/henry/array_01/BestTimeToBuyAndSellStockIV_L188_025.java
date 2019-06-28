7fb7517b6920e1e0675b9f15b423055270b65208
Henry <hongyu_98982@163.com>
2019-06-28 14:22:16 +0800

    188. Best Time to Buy and Sell Stock IV

---
+++ src/main/java/com/home/henry/array_01/BestTimeToBuyAndSellStockIV_L188_025.java
@@ -0,0 +1,36 @@
+package com.home.henry.array_01;
+
+/**
+ * 188. Best Time to Buy and Sell Stock IV
+ * 典型DP, 现在还没弄懂
+ */
+public class BestTimeToBuyAndSellStockIV_L188_025 {
+
+
+    class Solution {
+        public int maxProfit(int k, int[] prices) {
+            int n = prices.length;
+
+            if (k >= n / 2) {
+                // 这种情况下该问题退化为普通的股票交易问题
+                int maxProfit = 0;
+                for (int i = 1; i < prices.length; i++) {
+                    if (prices[i] > prices[i - 1]) {
+                        maxProfit += (prices[i] - prices[i - 1]);
+                    }
+                }
+                return maxProfit;
+            }
+
+            int[][] maxProfit = new int[k + 1][n];
+            for (int i = 1; i <= k; i++) {
+                int localMax = maxProfit[i - 1][0] - prices[0];
+                for (int j = 1; j < n; j++) {
+                    maxProfit[i][j] = Math.max(maxProfit[i][j - 1], prices[j] + localMax);
+                    localMax = Math.max(localMax, maxProfit[i - 1][j] - prices[j]);
+                }
+            }
+            return maxProfit[k][n - 1];
+        }
+    }
+}
