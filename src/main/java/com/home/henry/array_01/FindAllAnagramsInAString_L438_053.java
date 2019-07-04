8a6a046949e1608515b37e1083fd43c436b01e95
Henry <hongyu_98982@163.com>
2019-07-04 11:23:39 +0800

    438. Find All Anagrams in a String

--- src/main/java/com/home/henry/array_01/FindAllAnagramsInAString_L438_053.java
+++ src/main/java/com/home/henry/array_01/FindAllAnagramsInAString_L438_053.java
@@ -12,14 +12,18 @@ import java.util.List;
  *
  * 这个题目其实是String类型的, 放在这里是因为这个是典型sliding window的解法
  * 和567非常像, 可以用同样的方法解
- * 
+ *
  * 题意是找出p的permutation在s中的index, 返回index的list
  * 难度标记为简单, 应该是中等
+ *
+ * 解法2用的while方法
+ * https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
+ * 参考这个地址中的ndec09的帖子, 和解法1思想类似, 但是思路很不相同
  */
 public class FindAllAnagramsInAString_L438_053 {
 
     static class Solution {
-        public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> findAnagrams(String s, String p) {
             List<Integer> res = new ArrayList<>();
             if (s == null || p == null || p.length() > s.length()) {return res;}
             int sLen = s.length(), pLen = p.length();
@@ -30,7 +34,7 @@ public class FindAllAnagramsInAString_L438_053 {
             }
             if (countIsZero(count)) {res.add(0);}
             for (int i = pLen; i < sLen; i++) {
-                count[s.charAt(i) - 'a'] --;
+                count[s.charAt(i) - 'a']--;
                 count[s.charAt(i - pLen) - 'a']++;
                 if (countIsZero(count)) {
                     res.add(i - pLen + 1);
@@ -47,9 +51,43 @@ public class FindAllAnagramsInAString_L438_053 {
         }
     }
 
+    static class SolutionWhile {
+        List<Integer> findAnagrams(String s, String p) {
+            List<Integer> res = new ArrayList<>();
+            int s1 = s.length(), p1 = p.length();
+            if (p1 > s1) {return res;}
+            int[] count = new int[26];
+            int start = 0, end = 0, counter = 0;
+            for (int i = 0; i < p.length(); i++) {
+                count[p.charAt(i) - 'a']++;
+            }
+            while (end < s1) {
+                char c1 = s.charAt(end);
+                if (count[c1 - 'a'] > 0) {
+                    counter++;
+                }
+                count[c1 - 'a']--;
+                end++;
+                while (counter == p.length()) {
+                    char c2 = s.charAt(start);
+                    if (count[c2 - 'a'] == 0) {
+                        counter--;
+                        res.add(start);
+                    }
+                    count[c2 - 'a']++;
+                    start++;
+                }
+            }
+            return res;
+        }
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println("[0, 6] -> "+s.findAnagrams("cbaebabacd", "abc"));
-        System.out.println("[0, 1, 2] -> "+s.findAnagrams("abab", "ab"));
+        SolutionWhile sw = new SolutionWhile();
+        System.out.println("[0, 6] -> " + s.findAnagrams("cbaebabacd", "abc"));
+        System.out.println("[0, 6] -> " + sw.findAnagrams("cbaebabacd", "abc"));
+        System.out.println("[0, 1, 2] -> " + s.findAnagrams("abab", "ab"));
+        System.out.println("[0, 1, 2] -> " + sw.findAnagrams("abab", "ab"));
     }
 }
