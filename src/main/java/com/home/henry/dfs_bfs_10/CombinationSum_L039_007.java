0e2f363e81abb824b9e2d3fa2a42ace28dd3e80f
Henry <hongyu_98982@163.com>
2019-07-29 10:12:38 +0800

    39. Combination Sum

---
+++ src/main/java/com/home/henry/dfs_bfs_10/CombinationSum_L039_007.java
@@ -0,0 +1,40 @@
+package com.home.henry.dfs_bfs_10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 39. Combination Sum
+ */
+public class CombinationSum_L039_007 {
+    static class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (candidates == null) {return res;}
+            Arrays.sort(candidates);
+            helper(candidates, res, new ArrayList<>(), target, 0);
+            return res;
+        }
+
+        private void helper(int[] candidates, List<List<Integer>> res, List<Integer> comb, int remainTarget,
+                            int startIndex) {
+            // exit
+            if (remainTarget < 0) {
+                return;
+            }
+            if (remainTarget == 0) {
+                res.add(new ArrayList<>(comb));
+                return;
+            }
+            for (int i = startIndex; i < candidates.length; i++) {
+                if (i != 0 && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
+                comb.add(candidates[i]);
+                helper(candidates, res, comb, remainTarget - candidates[i], i);
+                comb.remove(comb.size() - 1);
+            }
+        }
+    }
+}
