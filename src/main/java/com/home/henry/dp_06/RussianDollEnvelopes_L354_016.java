1cc2538292c3ef4725d6c889973316a47c41e5f0
Henry <qq98982@gmail.com>
2019-12-23 00:24:00 +0800

    354. Russian Doll Envelopes

---
+++ src/main/java/com/home/henry/dp_06/RussianDollEnvelopes_L354_016.java
@@ -0,0 +1,46 @@
+package com.home.henry.dp_06;
+
+import java.util.Arrays;
+
+/**
+ * 354. Russian Doll Envelopes
+ * <p>
+ * You have a number of envelopes with widths and heights given as a pair of integers (w, h). One envelope can fit into another if and only if both the width and height of one envelope is greater than the width and height of the other envelope.
+ * What is the maximum number of envelopes can you Russian doll? (put one inside other)
+ * Note: Rotation is not allowed.
+ * <p>
+ * Example: Input: [[5,4],[6,4],[6,7],[2,3]] Output: 3
+ * Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+ * 9c dp video 4
+ */
+public class RussianDollEnvelopes_L354_016 {
+    class Solution {
+        public int maxEnvelopes(int[][] envelopes) {
+            if (envelopes == null || envelopes.length == 0) {
+                return 0;
+            }
+
+            int n = envelopes.length;
+            int[] f = new int[n];
+            Arrays.sort(envelopes, (a, b) -> {
+                if (a[0] == b[0]) {
+                    return a[1] - b[1];
+                } else {
+                    return a[0] - b[0];
+                }
+            });
+
+            int i, j, res = 0;
+            for (i = 0; i < n; i++) {
+                f[i] = 1;
+                for (j = 0; j < i; j++) {
+                    if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
+                        f[i] = Math.max(f[i], f[j] + 1);
+                    }
+                }
+                res = Math.max(res, f[i]);
+            }
+            return res;
+        }
+    }
+}
