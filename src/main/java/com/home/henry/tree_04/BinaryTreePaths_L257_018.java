bf2f2c06a422abc7c850b8240edb95f3aa7f5c2e
Henry <qq98982@gmail.com>
2019-12-31 10:58:48 +0800

    257. Binary Tree Paths

--- src/main/java/com/home/henry/tree_04/BinaryTreePaths_L257_018.java
+++ src/main/java/com/home/henry/tree_04/BinaryTreePaths_L257_018.java
@@ -3,6 +3,24 @@ package com.home.henry.tree_04;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 257. Binary Tree Paths
+ * Given a binary tree, return all root-to-leaf paths.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Input:
+ *
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ *
+ * Output: ["1->2->5", "1->3"]
+ *
+ * Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+ */
 public class BinaryTreePaths_L257_018 {
 
     public List<String> binaryTreePaths(TreeNode root) {
@@ -11,12 +29,15 @@ public class BinaryTreePaths_L257_018 {
         return res;
     }
 
-    private void helper(TreeNode root, String string, List<String> res) {
-        if (root == null) {return;}
+    private void helper(TreeNode root, String path, List<String> res) {
         if (root.left == null && root.right == null) {
-            res.add(string + root.val);
+            res.add(path + root.val);
         }
-        helper(root.left, string + root.val + "->", res);
-        helper(root.right, string + root.val + "->", res);
+        if (root.left != null) {
+            helper(root.left, path + root.val + "->", res);
+        }
+        if (root.right != null) {
+            helper(root.right, path + root.val + "->", res);
+        }
     }
 }
