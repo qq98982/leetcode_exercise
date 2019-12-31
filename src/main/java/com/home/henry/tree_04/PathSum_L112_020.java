ef6349792f93369dd29f477188b40b1606db75ea
Henry <qq98982@gmail.com>
2019-12-31 11:39:43 +0800

    112. Path Sum

--- src/main/java/com/home/henry/tree_04/PathSum_L112_020.java
+++ src/main/java/com/home/henry/tree_04/PathSum_L112_020.java
@@ -1,6 +1,9 @@
 package com.home.henry.tree_04;
 
+import java.util.Stack;
+
 /**
+ * 112. Path Sum
  * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
  */
 public class PathSum_L112_020 {
@@ -11,5 +14,28 @@ public class PathSum_L112_020 {
             if (root.val == sum) {return true;}
         }
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) {return false;}
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            if (curr.left == null && curr.right == null) {
+                if (curr.val == sum) {
+                    return true;
+                }
+            }
+            if (curr.right != null) {
+                stack.push(curr.right);
+                curr.right.val += curr.val;
+            }
+            if (curr.left != null) {
+                stack.push(curr.left);
+                curr.left.val += curr.val;
+            }
+        }
+        return false;
     }
 }
