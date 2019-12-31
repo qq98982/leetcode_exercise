4b718511662073c89edf0fe8c2f7cd018dcb47e1
Henry <qq98982@gmail.com>
2019-12-31 13:17:02 +0800

    113. Path Sum II

--- src/main/java/com/home/henry/tree_04/PathSumII_L113_021.java
+++ src/main/java/com/home/henry/tree_04/PathSumII_L113_021.java
@@ -9,6 +9,7 @@ import java.util.Stack;
  * 113. Path Sum II
  * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
  * Note: A leaf is a node with no children.
+ * 推荐第二个用ArrayList的解法
  */
 public class PathSumII_L113_021 {
 
@@ -33,4 +34,23 @@ public class PathSumII_L113_021 {
         history.pop();
     }
 
+
+    public List<List<Integer>> pathSumII(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper2(res, root, sum, new ArrayList<Integer>());
+        return res;
+    }
+
+    private void helper2(List<List<Integer>> res, TreeNode root, int sum, ArrayList<Integer> list) {
+        if (root == null) {return;}
+        list.add(root.val);
+        if (root.left == null && root.right == null) {
+            if (sum == root.val) {
+                res.add(new ArrayList<>(list));
+            }
+        }
+        helper2(res, root.left, sum - root.val, list);
+        helper2(res, root.right, sum - root.val, list);
+        list.remove(list.size() -1);
+    }
 }
