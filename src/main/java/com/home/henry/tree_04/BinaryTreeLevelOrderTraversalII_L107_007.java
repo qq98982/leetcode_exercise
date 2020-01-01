623c6dfe2e528cbce632f0cd5f7a545704553ae4
Henry <qq98982@gmail.com>
2020-01-01 17:59:34 +0800

    107. Binary Tree Level Order Traversal II

--- src/main/java/com/home/henry/tree_04/BinaryTreeLevelOrderTraversalII_L107_007.java
+++ src/main/java/com/home/henry/tree_04/BinaryTreeLevelOrderTraversalII_L107_007.java
@@ -1,25 +1,69 @@
 package com.home.henry.tree_04;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-
+/**
+ * 107. Binary Tree Level Order Traversal II
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ * <p>
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its bottom-up level order traversal as:
+ * [ [15,7], [9,20], [3] ]
+ *
+ * 是102的follow up, 注意使用了index 0
+ */
 public class BinaryTreeLevelOrderTraversalII_L107_007 {
 
-    // BFS
+    // DFS
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> res = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
         helper(res, root, 0);
         return res;
     }
 
     private void helper(List<List<Integer>> res, TreeNode root, int level) {
-        if (root == null) return;
-        if (level >= res.size()) {
-            res.add(0, new LinkedList<>());
+        if (root == null) {
+            return;
         }
-        helper(res,root.left, level + 1);
-        helper(res,root.right, level + 1);
+        if (level >= res.size()) {
+            res.add(0, new ArrayList<>());
+        }
         res.get(res.size() - level - 1).add(root.val);
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
+    }
+
+    // BFS
+    public List<List<Integer>> levelOrderBottomBFS(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr != null && curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr != null && curr.right != null) {
+                    queue.offer(curr.right);
+                    list.add(curr.val);
+                }
+            }
+            res.add(0, list);
+        }
+        return res;
     }
 }
