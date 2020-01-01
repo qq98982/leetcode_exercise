ce51ade6a5dab8ab6eb5858fc9d005ce46df681f
Henry <qq98982@gmail.com>
2020-01-01 12:11:18 +0800

    337. House Robber III

--- src/main/java/com/home/henry/tree_04/HouseRobberIII_L337_026.java
+++ src/main/java/com/home/henry/tree_04/HouseRobberIII_L337_026.java
@@ -1,10 +1,38 @@
 package com.home.henry.tree_04;
 
 /**
- * House Robber III
+ * 337. House Robber III
+ * The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root."
+ * Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that
+ * "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked
+ * houses were broken into on the same night.
+ *
+ * Input: [3,2,3,null,3,null,1]
+ *
+ *      3
+ *     / \
+ *    2   3
+ *     \   \
+ *      3   1
+ *
+ * Output: 7
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ *
+ * Input: [3,4,5,1,3,null,1]
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \   \
+ *  1   3   1
+ *
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ *
+ * Determine the maximum amount of money the thief can rob tonight without alerting the police.
  * dfs all the nodes of the tree, each node return two number, int[] num, num[0] is the max value while rob this node,
  * num[1] is max value while not rob this value. Current node return value only depend on its children's value.
- * 第五种 Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber III.
+ * 第一种 Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber III.
  * 第二个方法更容易看懂 Runtime: 592 ms, faster than 28.89%
  */
 public class HouseRobberIII_L337_026 {
@@ -38,6 +66,6 @@ public class HouseRobberIII_L337_026 {
 
     private int robExclude(TreeNode root) {
         if (root == null) {return 0;}
-        return robSec(root.left) + rob(root.right);
+        return robSec(root.left) + robSec(root.right);
     }
 }
