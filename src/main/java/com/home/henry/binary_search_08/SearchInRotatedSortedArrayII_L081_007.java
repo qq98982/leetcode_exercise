a653f8fbdf8a0b060e7a97b0047e0bc25340bba6
Henry <qq98982@gmail.com>
2020-01-15 11:03:24 +0800

    81. Search in Rotated Sorted Array II

--- src/main/java/com/home/henry/binary_search_08/SearchInRotatedSortedArrayII_L081_007.java
+++ src/main/java/com/home/henry/binary_search_08/SearchInRotatedSortedArrayII_L081_007.java
@@ -1,5 +1,20 @@
 package com.home.henry.binary_search_08;
 
+/**
+ * 81. Search in Rotated Sorted Array II
+ *
+ * Medium
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ *
+ * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return true, otherwise return false.
+ *
+ * Input: nums = [2,5,6,0,0,1,2], target = 0 Output: true
+ *
+ * Input: nums = [2,5,6,0,0,1,2], target = 3 Output: false
+ */
 class SearchInRotatedSortedArrayII_L081_007 {
     public boolean search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {return false;}
@@ -32,5 +47,43 @@ class SearchInRotatedSortedArrayII_L081_007 {
         }
         return false;
     }
-    
+
+}
+
+/**
+ * 这个方法是用的start + 1 < end, 二分法，要考虑边界情况[1,1,3,1]
+ */
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target || nums[start] == target || nums[end] == target) {
+                return true;
+            }
+            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+            } else if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {
+                if (target <= nums[end] && target >= nums[mid]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        if (nums[start] == target) {
+            return true;
+        }
+        return nums[end] == target;
+    }
 }
