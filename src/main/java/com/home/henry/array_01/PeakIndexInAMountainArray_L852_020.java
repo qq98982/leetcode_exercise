17d04a361af0417236b5c79ba19c873da03b9707
Henry <qq98982@gmail.com>
2020-01-15 18:42:35 +0800

    852. Peak Index in a Mountain Array

--- src/main/java/com/home/henry/array_01/PeakIndexInAMountainArray_L852_020.java
+++ src/main/java/com/home/henry/array_01/PeakIndexInAMountainArray_L852_020.java
@@ -4,7 +4,17 @@ import java.util.Stack;
 
 /**
  * 852. Peak Index in a Mountain Array
- * 第一种是stack方法 O(n) 第二种是Binary Search方法O(logN)
+ * Let's call an array A a mountain if the following properties hold:
+ *
+ * A.length >= 3
+ * There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+ * Given an array that is definitely a mountain, return any i such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+ *
+ * Input: [0,1,0] Output: 1
+ *
+ * Input: [0,2,1,0] Output: 1
+ *
+ * 第一种是stack方法 O(n) 第二种是Binary Search方法O(logN), 第三种是start + 1 < end的用法
  */
 public class PeakIndexInAMountainArray_L852_020 {
     static class Solution {
@@ -37,6 +47,21 @@ public class PeakIndexInAMountainArray_L852_020 {
                 }
             }
             return lo;
+        }
+    }
+
+    static class SolutionBS {
+        public int peakIndexInMountainArray(int[] A) {
+            int start = 0, end = A.length - 1;
+            while (start + 1 < end) {
+                int mid = (end - start) / 2 + start;
+                if (A[mid] < A[mid + 1]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+            return A[start] > A[end] ? start : end;
         }
     }
 }
