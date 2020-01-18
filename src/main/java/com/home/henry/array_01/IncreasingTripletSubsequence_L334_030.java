0558ba61c5f3a5d431ae59bd2f948ce280a82678
Henry <qq98982@gmail.com>
2020-01-18 16:10:26 +0800

    334. Increasing Triplet Subsequence

--- src/main/java/com/home/henry/array_01/IncreasingTripletSubsequence_L334_030.java
+++ src/main/java/com/home/henry/array_01/IncreasingTripletSubsequence_L334_030.java
@@ -2,7 +2,20 @@ package com.home.henry.array_01;
 
 /**
  * 334. Increasing Triplet Subsequence
- * 方法很巧妙, 使用了滚动更新min1和min2的方法
+ * Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+ *
+ * Formally the function should:
+ *
+ * Return true if there exists i, j, k
+ * such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+ * Note: Your algorithm should run in O(n) time complexity and O(1) space complexity.
+ *
+ * Input: [1,2,3,4,5] Output: true Example 2:
+ *
+ * Input: [5,4,3,2,1] Output: false
+ *
+ * 方法很巧妙, 使用了滚动更新min1和min2的方法, 最主要的地方是加了两个数字, 当前数字比其中一个大, 比另外一个小
+ *  如果比最大的还要大 那么就是需要的 按这个思路去更新容易写代码
  */
 public class IncreasingTripletSubsequence_L334_030 {
     class Solution {
