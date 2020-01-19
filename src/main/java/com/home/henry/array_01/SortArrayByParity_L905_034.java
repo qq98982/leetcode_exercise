713d11fc05ccafb8c0fce63b363878ddf431d139
Henry <qq98982@gmail.com>
2020-01-19 18:48:14 +0800

    905. Sort Array By Parity

--- src/main/java/com/home/henry/array_01/SortArrayByParity_L905_034.java
+++ src/main/java/com/home/henry/array_01/SortArrayByParity_L905_034.java
@@ -2,6 +2,13 @@ package com.home.henry.array_01;
 
 /**
  * 905. Sort Array By Parity
+ * Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed
+ * by all the odd elements of A.
+ *
+ * You may return any answer array that satisfies this condition.
+ *
+ * Input: [3,1,2,4] Output: [2,4,3,1]
+ * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
  * 排序一个array, 偶数在左, 奇数在右
  */
 public class SortArrayByParity_L905_034 {
