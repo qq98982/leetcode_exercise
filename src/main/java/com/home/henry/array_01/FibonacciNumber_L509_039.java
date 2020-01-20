04a81499439b84f675df07cf566abd2ac9aff769
Henry <qq98982@gmail.com>
2020-01-20 22:55:44 +0800

    509. Fibonacci Number

--- src/main/java/com/home/henry/array_01/FibonacciNumber_L509_039.java
+++ src/main/java/com/home/henry/array_01/FibonacciNumber_L509_039.java
@@ -2,6 +2,7 @@ package com.home.henry.array_01;
 
 /**
  * 509. Fibonacci Number
+ * 使用old, curr, now更好理解
  */
 public class FibonacciNumber_L509_039 {
     static class Solution {
@@ -14,6 +15,24 @@ public class FibonacciNumber_L509_039 {
                 sum = two + one;
             }
             return sum;
+        }
+        public int fib2(int N) {
+            if (N <= 1) {return N;}
+            int old = 1, curr = 1, now = 1;
+            for (int i = 3; i <= N; i++) {
+                old = curr;
+                curr = now;
+                now = old + curr;
+            }
+            return now;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(s.fib(i) == s.fib2(i));
         }
     }
 
