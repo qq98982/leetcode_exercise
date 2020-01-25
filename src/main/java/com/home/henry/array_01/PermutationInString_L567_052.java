144fb5da0ad600a9c14535f9e4bf6ffcf468c0ab
Henry <qq98982@gmail.com>
2020-01-25 13:26:33 +0800

    567. Permutation in String

--- src/main/java/com/home/henry/array_01/PermutationInString_L567_052.java
+++ src/main/java/com/home/henry/array_01/PermutationInString_L567_052.java
@@ -1,5 +1,8 @@
 package com.home.henry.array_01;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 567. Permutation in String
  *
@@ -17,6 +20,7 @@ package com.home.henry.array_01;
  * 将s1的count++, s2的--, 如果s1走完了, 如果count上所有的数字都是0, 那说明s2就是s1的permutation
  * 如果走完s1的长度了, 这个时候还没有得出结果, 那么从s2-s1的第一位开始--, 同时从第一位开始++, 有一个s1长度的差值, 这个时候
  * 完成一个数字就要判断是否count都为0, 因为有可能在小于s1长度的时候就有s1的部分值了
+ * 后一个方法是根据sliding window的模板更改的, 推荐
  */
 public class PermutationInString_L567_052 {
     static class Solution {
@@ -43,6 +47,40 @@ public class PermutationInString_L567_052 {
                 if (count[i] != 0) {return false;}
             }
             return true;
+        }
+    }
+    private static class SolutionTemplate {
+        public boolean checkInclusion(String s1, String s2) {
+            if (s1.length() > s2.length()) {return false;}
+            Map<Character, Integer> map = new HashMap<>();
+            for (Character cc : s1.toCharArray()) {
+                map.put(cc, map.getOrDefault(cc, 0) + 1);
+            }
+            int start = 0, end = 0, count = map.size();
+            while (end < s2.length()) {
+                char c = s2.charAt(end);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) - 1);
+                    if (map.get(c) == 0) {
+                        count--;
+                    }
+                }
+                end++;
+                while (count == 0) {
+                    char tempc = s2.charAt(start);
+                    if (map.containsKey(tempc)) {
+                        map.put(tempc, map.get(tempc) + 1);
+                        if (map.get(tempc) > 0) {
+                            count++;
+                        }
+                        if (end - start == s1.length()) {
+                            return true;
+                        }
+                    }
+                    start++;
+                }
+            }
+            return false;
         }
     }
 }
