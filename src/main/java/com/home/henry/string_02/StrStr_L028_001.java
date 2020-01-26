c74ba681be59e2b9ab6c865ef443f94e61d1707e
Henry <qq98982@gmail.com>
2020-01-26 13:45:09 +0800

    28. Implement strStr()

--- src/main/java/com/home/henry/string_02/StrStr_L028_001.java
+++ src/main/java/com/home/henry/string_02/StrStr_L028_001.java
@@ -1,6 +1,8 @@
 package com.home.henry.string_02;
 
 /**
+ * 28. Implement strStr()
+ *
  * Implement strStr(). Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
  */
 public class StrStr_L028_001 {
@@ -20,6 +22,21 @@ public class StrStr_L028_001 {
                     }
                 } else {
                     break;
+                }
+            }
+        }
+        return -1;
+    }
+    public int strStr2(String haystack, String needle) {
+        if (needle.length() == 0) {return 0;}
+        if (haystack.length() < needle.length()) {return -1;}
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if (needle.charAt(j) != haystack.charAt(i + j)) {
+                    break;
+                }
+                if (j == needle.length() - 1 + i) {
+                    return i;
                 }
             }
         }
