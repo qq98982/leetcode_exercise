81ff3be69d2dfb6b373a289cbf3e2e2cb1fe5b4d
Henry <qq98982@gmail.com>
2020-01-27 17:53:25 +0800

    383. Ransom Note

--- src/main/java/com/home/henry/string_02/RansomNote_L383_005.java
+++ src/main/java/com/home/henry/string_02/RansomNote_L383_005.java
@@ -1,10 +1,17 @@
 package com.home.henry.string_02;
 
 /**
+ * 383. Ransom Note
+ *
  * Given an arbitrary ransom note string and another string containing letters from all the magazines,
  * write a function that will return true if the ransom note can be constructed from the magazines ;
  * otherwise, it will return false.
  * Each letter in the magazine string can only be used once in your ransom note.
+ *
+ * You may assume that both strings contain only lowercase letters.
+ * canConstruct("a", "b") -> false
+ * canConstruct("aa", "ab") -> false
+ * canConstruct("aa", "aab") -> true
  */
 public class RansomNote_L383_005 {
 
