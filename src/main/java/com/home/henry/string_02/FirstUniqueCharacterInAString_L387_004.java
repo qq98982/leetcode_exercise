673039890db37bdf2c70b99ec142b7fc040d4c21
Henry <qq98982@gmail.com>
2020-01-27 10:28:06 +0800

    3. Longest Substring Without Repeating Characters

--- src/main/java/com/home/henry/string_02/FirstUniqueCharacterInAString_L387_004.java
+++ src/main/java/com/home/henry/string_02/FirstUniqueCharacterInAString_L387_004.java
@@ -1,5 +1,17 @@
 package com.home.henry.string_02;
 
+/**
+ * 387. First Unique Character in a String
+ *
+ * Given a string, find the first non-repeating character in it and return it's index.
+ * If it doesn't exist, return -1.
+ *
+ * s = "leetcode" return 0.
+ *
+ * s = "loveleetcode", return 2.
+ *
+ * Note: You may assume the string contain only lowercase letters.
+ */
 public class FirstUniqueCharacterInAString_L387_004 {
 
     public int firstUniqChar(String s) {
--- src/main/java/com/home/henry/string_02/LongestSubstringWithoutRepeatingCharacters_L003_022.java
+++ src/main/java/com/home/henry/string_02/LongestSubstringWithoutRepeatingCharacters_L003_022.java
@@ -4,30 +4,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 3. Longest Substring Without Repeating Characters
+ *
  * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * 按照Sliding window模板解题, 注意判断条件和顺序
  */
 public class LongestSubstringWithoutRepeatingCharacters_L003_022 {
 
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int begin = 0, end = 0, count = 0, res = 0;
-        while (end < s.length()) {
-            char c = s.charAt(end);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            if (map.get(c) > 1) {
-                count++;
-            }
-            end++;
-            while (count > 0) {
-                char tmp = s.charAt(begin);
-                if (map.get(tmp) > 1) {
-                    count--;
+    static class Solution {
+        public static int lengthOfLongestSubstring(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            int begin = 0, end = 0, count = 0, res = 0;
+            while (end < s.length()) {
+                char c = s.charAt(end);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                if (map.get(c) > 1) {
+                    count++;
                 }
-                map.put(tmp, map.get(tmp) - 1);
-                begin++;
+                end++;
+                while (count > 0) {
+                    char tmp = s.charAt(begin);
+                    if (map.get(tmp) > 1) {
+                        count--;
+                    }
+                    map.put(tmp, map.get(tmp) - 1);
+                    begin++;
+                }
+                res = Math.max(res, end - begin);
             }
-            res = Math.max(res, end - begin);
+            return res;
         }
-        return res;
     }
+
+    public static void main(String[] args) {
+        assert (Solution.lengthOfLongestSubstring("pkwwkew") == 3);
+        assert (Solution.lengthOfLongestSubstring("bbbbb") == 1);
+        assert (Solution.lengthOfLongestSubstring("abcabcbb") == 3);
+    }
+
 }
