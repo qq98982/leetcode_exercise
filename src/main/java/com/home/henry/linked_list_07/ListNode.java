320401ac509817af247e2d81f430b9f8e3808adb
Henry <qq98982@gmail.com>
2020-02-14 09:49:58 +0800

    ListNode toString() method

--- src/main/java/com/home/henry/linked_list_07/ListNode.java
+++ src/main/java/com/home/henry/linked_list_07/ListNode.java
@@ -7,4 +7,46 @@ public class ListNode {
     ListNode next;
 
     ListNode(int x) { val = x; }
+
+    @Override
+    public String toString() {
+        ListNode fast = this;
+        ListNode slow = this;
+        boolean circle = false;
+        ListNode cross = this;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                circle = true;
+                fast = this;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                cross = fast;
+                break;
+            }
+        }
+        if (!circle) {
+            return val + " -> " + next;
+        } else {
+            slow = this;
+            StringBuilder sb = new StringBuilder();
+            sb.append(slow.val).append(" -> ");
+            while (slow != cross) {
+                slow = slow.next;
+                sb.append(slow.val).append(" -> ");
+            }
+            slow = slow.next;
+            sb.append(slow.val);
+            while (slow.next != cross) {
+                sb.append(" -> ");
+                slow = slow.next;
+                sb.append(slow.val);
+            }
+            sb.append(" -> ").append(cross.val);
+            return sb.toString();
+        }
+    }
 }
