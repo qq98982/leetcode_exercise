46671c929babc7a1e77fee288efa3b24736bbd5f
Henry <qq98982@gmail.com>
2020-02-22 11:36:59 +0800

    704. Binary Search

--- src/main/java/com/home/henry/binary_search_08/BinarySearch_L704_003.java
+++ src/main/java/com/home/henry/binary_search_08/BinarySearch_L704_003.java
@@ -22,4 +22,41 @@ public class BinarySearch_L704_003 {
         return -1;
     }
 
+    // 最常见方式
+    public int search2(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {return -1;}
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    // start + 1 < end 方式
+    public int search3(int[] nums, int target) {
+        if (nums == null || nums.length == 0
+            || nums[0] > target || nums[nums.length - 1] < target) {return -1;}
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[start] == target) {return start;}
+        else if (nums[end] == target) {return end;}
+        else {return -1;}
+    }
+
 }
