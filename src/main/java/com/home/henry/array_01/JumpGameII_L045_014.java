aa8d13362443bcdea5a85f3cf8c1ce9f6ac6255e
Henry <qq98982@gmail.com>
2020-04-04 15:33:49 +0800

    45. Jump Game II

--- src/main/java/com/home/henry/array_01/JumpGameII_L045_014.java
+++ src/main/java/com/home/henry/array_01/JumpGameII_L045_014.java
@@ -13,6 +13,7 @@ public class JumpGameII_L045_014 {
         public int jump(int[] nums) {
             if (nums == null || nums.length < 2) {return 0;}
             int currMax = 0, maxNext = 0, res = 0;
+            // 因为是如果到了最后一步, 不能再++了, 所以这里只要到nums.length - 1
             for (int i = 0; i < nums.length - 1; i++) {
                 maxNext = Math.max(maxNext, nums[i] + i);
                 if (i == currMax) {
@@ -33,11 +34,12 @@ public class JumpGameII_L045_014 {
             int level = 0, currMax = 0, maxNext = 0, i = 0;
             while (currMax - i + 1 > 0) {
                 level++;
-                for (; i <= currMax; i++) {
+                while (i <= currMax) {
                     maxNext = Math.max(currMax, nums[i] + i);
                     if (maxNext >= nums.length - 1) {
                         return level;
                     }
+                    i++;
                 }
                 currMax = maxNext;
             }
