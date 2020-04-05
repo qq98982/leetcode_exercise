8cae38644cb1d5d808014c1ebf2d7f009c765ff7
Henry <qq98982@gmail.com>
2020-04-05 09:27:38 +0800

    33. Search in Rotated Sorted Array

--- src/main/java/com/home/henry/array_01/SearchInRotatedSortedArray_L033_016.java
+++ src/main/java/com/home/henry/array_01/SearchInRotatedSortedArray_L033_016.java
@@ -2,6 +2,15 @@ package com.home.henry.array_01;
 
 /**
  * 33. Search in Rotated Sorted Array
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 0 Output: 4
+ * Input: nums = [4,5,6,7,0,1,2], target = 3 Output: -1
  */
 public class SearchInRotatedSortedArray_L033_016 {
 
@@ -21,7 +30,7 @@ public class SearchInRotatedSortedArray_L033_016 {
                     //target 在这段里
                     if (target >= nums[left] && target < nums[mid]) {
                         right = mid - 1;
-                    }else {
+                    } else {
                         left = mid + 1;
                     }
                     //右半段是有序的
@@ -34,6 +43,42 @@ public class SearchInRotatedSortedArray_L033_016 {
                 }
             }
             return nums[left] == target ? left : -1;
+        }
+    }
+
+    static class Solution2 {
+        // at least ons part is sorted
+        public int search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int start = 0, end = nums.length - 1;
+            while (start + 1 < end) {
+                int mid = (end - start) / 2 + start;
+                if (nums[mid] == target) {return mid;}
+                if (nums[start] < nums[mid]) {
+                    // left part is sorted
+                    if (nums[start] <= target && target <= nums[mid]) {
+                        end = mid;
+                    } else {
+                        start = mid;
+                    }
+                } else {
+                    // right part is sorted
+                    if (nums[mid] <= target && target <= nums[end]) {
+                        start = mid;
+                    } else {
+                        end = mid;
+                    }
+                }
+            }
+            if (nums[start] == target) {
+                return start;
+            }
+            if (nums[end] == target) {
+                return end;
+            }
+            return -1;
         }
     }
 }
