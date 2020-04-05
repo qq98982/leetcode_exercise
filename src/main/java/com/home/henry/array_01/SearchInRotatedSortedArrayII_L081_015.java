e25180f190fb2f0cf17fc972a0f08787f98dab01
Henry <qq98982@gmail.com>
2020-04-05 11:21:49 +0800

    81. Search in Rotated Sorted Array II

--- src/main/java/com/home/henry/array_01/SearchInRotatedSortedArrayII_L081_015.java
+++ src/main/java/com/home/henry/array_01/SearchInRotatedSortedArrayII_L081_015.java
@@ -2,9 +2,20 @@ package com.home.henry.array_01;
 
 /**
  * 81. Search in Rotated Sorted Array II
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+ * You are given a target value to search. If found in the array return true, otherwise return false.
+ *
+ * Input: nums = [2,5,6,0,0,1,2], target = 0 Output: true
+ * Input: nums = [2,5,6,0,0,1,2], target = 3 Output: false
+ *
+ * Follow up:
+ * This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+ * Would this affect the run-time complexity? How and why?
  */
 public class SearchInRotatedSortedArrayII_L081_015 {
-    class Solution {
+    static class Solution {
         public boolean search(int[] nums, int target) {
             if (nums == null || nums.length == 0) {return false;}
 
@@ -34,6 +45,36 @@ public class SearchInRotatedSortedArrayII_L081_015 {
                     }
                 }
             }
+            return false;
+        }
+    }
+
+    static class Solution2 {
+        // it may contains duplicate
+        public boolean search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {return false;}
+            int start = 0, end = nums.length - 1;
+            while (start + 1 < end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == target || nums[start] == target || nums[end] == target) {return true;}
+                if (nums[start] == nums[mid] && nums[mid] == nums[end]) { // to avoid [1,1,3,1] case
+                    start++;
+                    end--;
+                } else if (nums[start] <= nums[mid]) {// left part is sorted, mid is in left
+                    if (nums[start] <= target && nums[mid] >= target) {
+                        end = mid;
+                    } else {
+                        start = mid;
+                    }
+                } else { // right part is sorted
+                    if (nums[end] >= target && nums[mid] <= target) {
+                        start = mid;
+                    } else {
+                        end = mid;
+                    }
+                }
+            }
+            if (nums[start] == target || nums[end] == target) {return true;}
             return false;
         }
     }
