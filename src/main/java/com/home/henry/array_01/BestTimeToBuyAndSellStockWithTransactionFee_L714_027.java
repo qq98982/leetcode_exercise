cc5a3f6981e5bd7e6e98da5a2b59885fffb53074
Henry <qq98982@gmail.com>
2020-04-07 19:25:17 +0800

    714. Best Time to Buy and Sell Stock with Transaction Fee

--- src/main/java/com/home/henry/array_01/BestTimeToBuyAndSellStockWithTransactionFee_L714_027.java
+++ src/main/java/com/home/henry/array_01/BestTimeToBuyAndSellStockWithTransactionFee_L714_027.java
@@ -9,18 +9,18 @@ public class BestTimeToBuyAndSellStockWithTransactionFee_L714_027 {
         public int maxProfit(int[] prices, int fee) {
             int N = prices.length;
             int[] buy = new int[N];
-            int[] s1 = new int[N];
+            int[] b1 = new int[N];
             int[] sell = new int[N];
-            int[] s2 = new int[N];
-            s1[0] = buy[0] = -prices[0];
-            sell[0] = s2[0] = 0;
+            int[] s1 = new int[N];
+            b1[0] = buy[0] = -prices[0];
+            sell[0] = s1[0] = 0;
             for (int i = 1; i < N; i++) {
-                buy[i] = Math.max(sell[i - 1], s2[i - 1]) - prices[i];
-                s1[i] = Math.max(buy[i - 1], s1[i - 1]);
-                sell[i] = Math.max(buy[i - 1], s1[i - 1]) - fee + prices[i];
-                s2[i] = Math.max(s2[i - 1], sell[i - 1]);
+                buy[i] = Math.max(sell[i - 1], s1[i - 1]) - prices[i];
+                b1[i] = Math.max(buy[i - 1], b1[i - 1]);
+                sell[i] = Math.max(buy[i - 1], b1[i - 1]) - fee + prices[i];
+                s1[i] = Math.max(s1[i - 1], sell[i - 1]);
             }
-            return Math.max(sell[N - 1], s2[N - 1]);
+            return Math.max(sell[N - 1], s1[N - 1]);
         }
     }
 
@@ -33,7 +33,7 @@ public class BestTimeToBuyAndSellStockWithTransactionFee_L714_027 {
             int[] buy = new int[len + 1];
             int[] sell = new int[len + 1];
             buy[1] = -prices[0];
-            for (int i = 1; i <= len; i++){
+            for (int i = 1; i <= len; i++) {
                 int price = prices[i - 1];
                 buy[i] = Math.max(buy[i - 1], sell[i - 1] - price);
                 sell[i] = Math.max(sell[i - 1], buy[i - 1] + price - fee);
@@ -43,10 +43,10 @@ public class BestTimeToBuyAndSellStockWithTransactionFee_L714_027 {
     }
 
     public static void main(String[] args) {
-        System.out.println(Solution2.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
-        System.out.println(Solution2.maxProfit(new int[]{1, 4}, 1));
-        System.out.println(Solution2.maxProfit(new int[]{1, 3}, 2));
-        System.out.println(Solution2.maxProfit(new int[]{1, 1}, 2));
-        System.out.println(Solution2.maxProfit(new int[]{1, 2}, 2));
+        System.out.println(Solution2.maxProfit(new int[] { 1, 3, 2, 8, 4, 9 }, 2));
+        System.out.println(Solution2.maxProfit(new int[] { 1, 4 }, 1));
+        System.out.println(Solution2.maxProfit(new int[] { 1, 3 }, 2));
+        System.out.println(Solution2.maxProfit(new int[] { 1, 1 }, 2));
+        System.out.println(Solution2.maxProfit(new int[] { 1, 2 }, 2));
     }
 }
