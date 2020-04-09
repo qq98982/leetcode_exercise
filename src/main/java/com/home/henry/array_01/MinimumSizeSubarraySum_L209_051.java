ef9f3ffe031c2d8c3aed63d0cfefc66337c81bf3
Henry <qq98982@gmail.com>
2020-04-09 18:08:13 +0800

    209. Minimum Size Subarray Sum

--- src/main/java/com/home/henry/array_01/MinimumSizeSubarraySum_L209_051.java
+++ src/main/java/com/home/henry/array_01/MinimumSizeSubarraySum_L209_051.java
@@ -2,9 +2,19 @@ package com.home.henry.array_01;
 
 /**
  * 209. Minimum Size Subarray Sum
- * 这个题目是找出一个连续的数组, 和加起来 >=s
- * 扫描线做法O(n)
  *
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous
+ * subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+ *
+ * Input: s = 7, nums = [2,3,1,2,4,3] Output: 2
+ * Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+ *
+ * Follow up:
+ * If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+ *
+ * 这个题目是找出一个连续的数组, 和加起来 >=s, 这个数组的数目最小的个数是多少
+ *
+ * 扫描线做法O(n)
  * 每次循环, 加上当前的值, 当这个相加和已经符合要求
  * 在while循环中判断连续是不是最小的, 然后再减去最左边的值, 最左边的index向右+1
  * while循环完成, 当前最小的就知道了
