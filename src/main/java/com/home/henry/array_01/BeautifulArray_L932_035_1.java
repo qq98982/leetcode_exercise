9c04bbe9a0fd2e70db956ef689571b303ec56b8d
Henry <qq98982@gmail.com>
2020-04-09 20:03:16 +0800

    932. Beautiful Array

---
+++ src/main/java/com/home/henry/array_01/BeautifulArray_L932_035_1.java
@@ -0,0 +1,48 @@
+package com.home.henry.array_01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 932. Beautiful Array
+ *
+ * For some fixed N, an array A is beautiful if it is a permutation of the integers 1, 2, ..., N, such that:
+ *
+ * For every i < j, there is no k with i < k < j such that A[k] * 2 = A[i] + A[j].
+ *
+ * Given N, return any beautiful array A.  (It is guaranteed that one exists.)
+ *
+ * Input: 4 Output: [2,1,4,3]
+ * Input: 5 Output: [3,1,2,5,4]
+ * 这个题目的意思比较奇怪, 用迭代的方式, 求出结果
+ */
+public class BeautifulArray_L932_035_1 {
+
+    static class Solution {
+        public static int[] beautifulArray(int N) {
+            if (N < 1) {return new int[] {};}
+            List<Integer> res = new ArrayList<>();
+            res.add(1);
+            while (res.size() < N) {
+                List<Integer> tmp = new ArrayList<>();
+                for (int n : res) {
+                    if (2 * n - 1 <= N) {
+                        tmp.add(2 * n - 1);
+                    }
+                }
+                for (int n : res) {
+                    if (2 * n <= N) {
+                        tmp.add(2 * n);
+                    }
+                }
+                res = tmp;
+            }
+            return res.stream().mapToInt(i -> i).toArray();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Solution.beautifulArray(8)));
+    }
+}
