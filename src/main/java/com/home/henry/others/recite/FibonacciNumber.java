fb95b81166b100214983d66431f45acd8ded6e8d
Henry <qq98982@gmail.com>
2020-04-10 10:40:09 +0800

    FibonacciNumber

---
+++ src/main/java/com/home/henry/others/recite/FibonacciNumber.java
@@ -0,0 +1,14 @@
+package com.home.henry.others.recite;
+
+class FibonacciNumber {
+    public int fib(int N) {
+        if (N <= 1) {return N;}
+        int old = 1, curr = 1, now = 1;
+        for (int i = 3; i <= N; i++) FibonacciNumber{
+            old = curr;
+            curr = now;
+            now = old + curr;
+        }
+        return now;
+    }
+}
