389232ca36dfbf3eb2474eba0156bb6d597f35f1
Henry <qq98982@gmail.com>
2020-04-10 10:40:34 +0800

    532. K-diff Pairs in an Array

--- src/main/java/com/home/henry/others/recite/FibonacciNumber.java
+++ src/main/java/com/home/henry/others/recite/FibonacciNumber.java
@@ -4,7 +4,7 @@ class FibonacciNumber {
     public int fib(int N) {
         if (N <= 1) {return N;}
         int old = 1, curr = 1, now = 1;
-        for (int i = 3; i <= N; i++) FibonacciNumber{
+        for (int i = 3; i <= N; i++) {
             old = curr;
             curr = now;
             now = old + curr;
