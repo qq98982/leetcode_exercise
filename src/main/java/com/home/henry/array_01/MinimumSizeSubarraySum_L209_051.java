e0ed72c0e3870c29dede4391b94fe8799b0c59ff
Henry <qq98982@gmail.com>
2020-04-17 21:53:51 +0800

    209. Minimum Size Subarray Sum

--- src/main/java/com/home/henry/array_01/MinimumSizeSubarraySum_L209_051.java
+++ src/main/java/com/home/henry/array_01/MinimumSizeSubarraySum_L209_051.java
@@ -28,12 +28,45 @@ public class MinimumSizeSubarraySum_L209_051 {
                 sum += nums[i];
                 while (left <= i && sum >= s) {
                     res = Math.min(res, i - left + 1);
-                    sum -= nums[left];
-                    left++;
+                    sum -= nums[left++];
+                    if (res == 1) {return 1;}
                 }
             }
             return res == Integer.MAX_VALUE ? 0 : res;
         }
     }
 
+    /**
+     * 二分法, 生成一个nums.length+1长度的数组, 做为相加使用
+     * 利用了相加和为递增的特点, 进行二分查找
+     * time: O(nlogn) space: O(n)
+     */
+    static class SolutionBS {
+        public int minSubArrayLen(int s, int[] nums) {
+            if (nums == null || nums.length == 0) {return 0;}
+            int[] sums = new int[nums.length + 1];
+            for (int i = 1; i < sums.length; i++) {
+                sums[i] = sums[i - 1] + nums[i - 1];
+            }
+            int minLen = Integer.MAX_VALUE;
+            for (int i = 0; i < sums.length; i++) {
+                int end = binarySearch(i + 1, sums.length - 1, sums[i] + s, sums);
+                if (end == sums.length) {break;}
+                minLen = Math.min(minLen, end - i);
+            }
+            return minLen == Integer.MAX_VALUE ? 0 : minLen;
+        }
+
+        private int binarySearch(int start, int end, int target, int[] sums) {
+            while (start <= end) {
+                int mid = (end - start) / 2 + start;
+                if (sums[mid] >= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return start;
+        }
+    }
 }
