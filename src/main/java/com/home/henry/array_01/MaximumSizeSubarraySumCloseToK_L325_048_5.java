504da0a871879c4d218d333534fb5073c6a2aa63
Henry <qq98982@gmail.com>
2020-04-18 10:10:36 +0800

    Maximum Size Subarray Sum Close To k

---
+++ src/main/java/com/home/henry/array_01/MaximumSizeSubarraySumCloseToK_L325_048_5.java
@@ -0,0 +1,32 @@
+package com.home.henry.array_01;
+
+import java.util.TreeSet;
+
+/**
+ * Maximum Size Subarray Sum Close To k
+ *
+ * 325的类似题目, Given an array, find the maximum sum of subarray close to k but not larger than k.
+ */
+public class MaximumSizeSubarraySumCloseToK_L325_048_5 {
+    static class Solution {
+        public int maxSubarrayCloseToK(int[] nums, int k) {
+            int res = Integer.MIN_VALUE;
+            // 这里要用TreeSet<Integer>, 否则没有ceiling方法
+            TreeSet<Integer> set = new TreeSet<>();
+            // init很重要
+            set.add(0);
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+                // ceil是比sum - k稍微大点的数, 所以后面sum减去ceil就是最接近k的数
+                Integer ceil = set.ceiling(sum - k);
+                if (ceil != null) {
+                    res = Math.max(sum - ceil, res);
+                }
+                set.add(sum);
+            }
+            return res == Integer.MIN_VALUE ? 0 : res;
+        }
+    }
+
+}
