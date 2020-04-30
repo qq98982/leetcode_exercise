51077bba3f2ee7c41e09bfe78460164e115dfd6b
Henry <qq98982@gmail.com>
2020-04-30 10:55:18 +0800

    124. Binary Tree Maximum Path Sum

--- src/main/java/com/home/henry/tree_04/BinaryTreeMaximumPathSum_L124_023.java
+++ src/main/java/com/home/henry/tree_04/BinaryTreeMaximumPathSum_L124_023.java
@@ -5,6 +5,8 @@ package com.home.henry.tree_04;
  * Given a non-empty binary tree, find the maximum path sum.
  * For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree
  * along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+ *
+ * 涉及到tree中的拆线, 大部分为post order方法, 这个就是这样的
  */
 public class BinaryTreeMaximumPathSum_L124_023 {
 
