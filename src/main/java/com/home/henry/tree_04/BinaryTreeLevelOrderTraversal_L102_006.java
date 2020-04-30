9244b7ae4b21c5539769c58b636e86d4d947c831
Henry <qq98982@gmail.com>
2020-04-30 10:16:01 +0800

    102. Binary Tree Level Order Traversal

--- src/main/java/com/home/henry/tree_04/BinaryTreeLevelOrderTraversal_L102_006.java
+++ src/main/java/com/home/henry/tree_04/BinaryTreeLevelOrderTraversal_L102_006.java
@@ -5,6 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * 102. Binary Tree Level Order Traversal
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ *
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its level order traversal as:
+ * [ [3], [9,20], [15,7] ]
+ */
 public class BinaryTreeLevelOrderTraversal_L102_006 {
 
     public List<List<Integer>> levelOrderBFS(TreeNode root) {
@@ -42,18 +56,11 @@ public class BinaryTreeLevelOrderTraversal_L102_006 {
     }
 
     private void levelOrderHelper(List<List<Integer>> res, TreeNode root, int level) {
-        if (root == null) {
-            return;
-        }
-        List<Integer> curr;
+        if (root == null) { return;}
         if (level >= res.size()) {
-            curr = new ArrayList<>();
-            curr.add(root.val);
-            res.add(curr);
-        } else {
-            curr = res.get(level);
-            curr.add(root.val);
+            res.add(new ArrayList<>());
         }
+        res.get(level).add(root.val);
         levelOrderHelper(res, root.left, level + 1);
         levelOrderHelper(res, root.right, level + 1);
     }
