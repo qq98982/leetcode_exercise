646a1beb564471dc41a4a194026d757a06bf8adb
Henry <qq98982@gmail.com>
2020-05-05 09:32:45 +0800

    572. Subtree of Another Tree

--- src/main/java/com/home/henry/tree_04/UnivaluedBinaryTree_L965_016.java
+++ src/main/java/com/home/henry/tree_04/UnivaluedBinaryTree_L965_016.java
@@ -1,15 +1,23 @@
 package com.home.henry.tree_04;
 
+/**
+ * 572. Subtree of Another Tree
+ *
+ * A binary tree is univalued if every node in the tree has the same value.
+ * Return true if and only if the given tree is univalued.
+ *
+ * Input: [1,1,1,1,1,null,1]
+ * Output: true
+ *
+ * Input: [2,2,2,5,2]
+ * Output: false
+ */
 public class UnivaluedBinaryTree_L965_016 {
 
     public boolean isUnivalTree(TreeNode root) {
         if (root == null) {return true;}
         return (root.left == null || root.left.val == root.val && isUnivalTree(root.left)) && (
-                root.right == null ||
-                root.right.val
-                == root.val
-                && isUnivalTree(
-                        root.right));
+                root.right == null || root.right.val == root.val && isUnivalTree(root.right));
     }
 
 }
