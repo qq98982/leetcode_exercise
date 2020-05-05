967fe19d1e4d8fcfa7dc976d9f6beec16d53f951
Henry <qq98982@gmail.com>
2020-05-05 09:22:51 +0800

    572. Subtree of Another Tree

--- src/main/java/com/home/henry/tree_04/SubtreeAnotherTree_L572_015.java
+++ src/main/java/com/home/henry/tree_04/SubtreeAnotherTree_L572_015.java
@@ -1,5 +1,40 @@
 package com.home.henry.tree_04;
 
+/**
+ * 572. Subtree of Another Tree
+ *
+ * Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node
+ * values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's
+ * descendants. The tree s could also be considered as a subtree of itself.
+ *
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ * Return true, because t has the same structure and node values with a subtree of s.
+ *
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ * Return false.
+ */
 public class SubtreeAnotherTree_L572_015 {
 
     public boolean isSubtree(TreeNode s, TreeNode t) {
