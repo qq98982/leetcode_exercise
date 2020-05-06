e52b243e8318faaf2ced6d0ab5f36943fc6d4c9d
Henry <qq98982@gmail.com>
2020-05-06 09:27:10 +0800

    968. Binary Tree Cameras

--- src/main/java/com/home/henry/tree_04/BinaryTreeCameras_L968_027.java
+++ src/main/java/com/home/henry/tree_04/BinaryTreeCameras_L968_027.java
@@ -1,10 +1,31 @@
 package com.home.henry.tree_04;
 
 /**
- * Apply a recusion function dfs.
+ * 968. Binary Tree Cameras
+ *
+ * Given a binary tree, we install cameras on the nodes of the tree.
+ * Each camera at a node can monitor its parent, itself, and its immediate children.
+ * Calculate the minimum number of cameras needed to monitor all nodes of the tree.
+ *
+ * Input: [0,0,null,0,0]
+ * Output: 1
+ * Explanation: One camera is enough to monitor all nodes if placed as shown.
+ *
+ * Input: [0,0,null,0,null,0,null,null,0]
+ * Output: 2
+ * Explanation: At least two cameras are needed to monitor all nodes of the tree.
+ * The above image shows one of the valid configurations of camera placement.
+ *
+ * Note:
+ * The number of nodes in the given tree will be in the range [1, 1000].
+ * Every node has value 0.
+ *
+ * ##########################################
+ *
+ * Apply a recursion function dfs.
  * Return 0 if it's a leaf.
  * Return 1 if it's a parent of a leaf, with a camera on this node.
- * Return 2 if it's coverd, without a camera on this node.
+ * Return 2 if it's covered, without a camera on this node.
  *
  * For each node,
  * if it has a child, which is leaf (node 0), then it needs camera.
@@ -13,23 +34,30 @@ package com.home.henry.tree_04;
  * If it needs camera, then res++ and we return 1.
  * If it's covered, we return 2.
  * Otherwise, we return 0.
+ *
+ * 用的是post order的方法
  */
 public class BinaryTreeCameras_L968_027 {
 
-    private int res = 0;
+    static class Solution {
 
-    public int minCameraCover(TreeNode root) {
-        return (helper(root) < 1 ? 1 : 0) + res;
-    }
+        private int res = 0;
 
-    private int helper(TreeNode root) {
-        if (root == null) {return 2;}
-        int left = helper(root.left);
-        int right = helper(root.right);
-        if (left == 0 || right == 0) {
-            res++;
-            return 1;
+        public int minCameraCover(TreeNode root) {
+            return (helper(root) < 1 ? 1 : 0) + res;
         }
-        return (left == 1 || right == 1) ? 2 : 0;
+
+        private int helper(TreeNode root) {
+            if (root == null) {return 2;}
+            int left = helper(root.left);
+            int right = helper(root.right);
+            if (left == 0 || right == 0) {
+                res++;
+                return 1;
+            }
+            return (left == 1 || right == 1) ? 2 : 0;
+        }
+
     }
+
 }
