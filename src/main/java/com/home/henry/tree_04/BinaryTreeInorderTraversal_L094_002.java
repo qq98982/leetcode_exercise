d05a3fe738da7f27fcc360ffb7d4bb049b0ae65f
Henry <qq98982@gmail.com>
2020-05-07 12:00:04 +0800

    94. Binary Tree Inorder Traversal

--- src/main/java/com/home/henry/tree_04/BinaryTreeInorderTraversal_L094_002.java
+++ src/main/java/com/home/henry/tree_04/BinaryTreeInorderTraversal_L094_002.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Stack;
 
 /**
+ * 94. Binary Tree Inorder Traversal
+ *
  * Given a binary tree, return the in order traversal of its nodes' values.
  *      1
  *    2   3
@@ -12,36 +14,44 @@ import java.util.Stack;
  * (a) Inorder (Left, Root, Right) : 4 2 5 1 3
  * (b) Preorder (Root, Left, Right) : 1 2 4 5 3
  * (c) Postorder (Left, Right, Root) : 4 5 2 3 1
+ *
+ * 下面的可以做为in order的模板
  */
 public class BinaryTreeInorderTraversal_L094_002 {
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        helper(res, root);
-        return res;
-    }
-
-    private void helper(List<Integer> res, TreeNode node) {
-        if (null != node) {
-            helper(res, node.left);
-            res.add(node.val);
-            helper(res, node.right);
+    static class SolutionRecur {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            helper(res, root);
+            return res;
         }
-    }
 
-    public List<Integer> inorderTraversalIterative(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = root;
-        while (null != cur || !stack.isEmpty()) {
-            while (null != cur) {
-                stack.add(cur);
-                cur = cur.left;
+        private void helper(List<Integer> res, TreeNode node) {
+            if (null != node) {
+                helper(res, node.left);
+                res.add(node.val);
+                helper(res, node.right);
             }
-            cur = stack.pop();
-            res.add(cur.val);
-            cur = cur.right;
         }
-        return res;
+    }
+
+    static class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            if (root == null) {return res;}
+            TreeNode curr = root;
+            Stack<TreeNode> stack = new Stack<>();
+            while (curr != null || !stack.isEmpty()) {
+                if (curr != null) {
+                    stack.push(curr);
+                    curr = curr.left;
+                } else {
+                    curr = stack.pop();
+                    res.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            return res;
+        }
     }
 }
