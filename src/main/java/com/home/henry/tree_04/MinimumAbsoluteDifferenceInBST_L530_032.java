932a1884a1051c4c1ca995b6748473bae0d918ae
Henry <qq98982@gmail.com>
2020-05-07 12:00:28 +0800

    530. Minimum Absolute Difference in BST

--- src/main/java/com/home/henry/tree_04/MinimumAbsoluteDifferenceInBST_L530_032.java
+++ src/main/java/com/home/henry/tree_04/MinimumAbsoluteDifferenceInBST_L530_032.java
@@ -1,30 +1,92 @@
 package com.home.henry.tree_04;
 
+import java.util.Stack;
+
 /**
  * 530. Minimum Absolute Difference in BST
  *
- * Since this is a BST, the inorder traversal of its nodes results in a sorted list of values. Thus, the minimum
- * absolute difference must occur in any adjacently traversed nodes. I use the global variable "prev" to
- * keep track of each node's inorder predecessor.
+ * Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
+ *
+ * Input:
+ *    1
+ *     \
+ *      3
+ *     /
+ *    2
+ * Output: 1
+ *
+ * Explanation:
+ * The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+ * test case : [236,104,701,null,227,null,911] [5,4,7] [1,null,3,2]
+ *
+ * 第一个方法中加入了两个共同变量, 第二个只要一个, 两个都是in order
  */
 public class MinimumAbsoluteDifferenceInBST_L530_032 {
 
-    private int minDiff = Integer.MAX_VALUE;
+    static class Solution {
+        private int minDiff = Integer.MAX_VALUE;
 
-    private TreeNode prev;
+        private TreeNode prev;
 
-    public int getMinimumDifference(TreeNode root) {
-        inOrder(root);
-        return minDiff;
-    }
-
-    private void inOrder(TreeNode root) {
-        if (root == null) {return;}
-        inOrder(root.left);
-        if (prev != null) {
-            minDiff = Math.min(minDiff, root.val - prev.val);
+        public int getMinimumDifference(TreeNode root) {
+            inOrder(root);
+            return minDiff;
         }
-        prev = root;
-        inOrder(root.right);
+
+        private void inOrder(TreeNode root) {
+            if (root == null) {return;}
+            inOrder(root.left);
+            if (prev != null) {
+                minDiff = Math.min(minDiff, root.val - prev.val);
+            }
+            prev = root;
+            inOrder(root.right);
+        }
     }
+
+    static class SolutionThreeParam {
+
+        int diff = Integer.MAX_VALUE;
+
+        public int getMinimumDifference(TreeNode root) {
+            helper(root, null, null);
+            return diff;
+        }
+
+        private void helper(TreeNode root, Integer min, Integer max) {
+            if (root == null) {return;}
+            helper(root.left, min, root.val);
+            if (min != null) {
+                diff = Math.min(diff, root.val - min);
+            }
+            if (max != null) {
+                diff = Math.min(diff, max - root.val);
+            }
+            helper(root.right, root.val, max);
+        }
+    }
+
+    // in order 模板修改
+    static class SolutionIter {
+        public int getMinimumDifference(TreeNode root) {
+            Stack<TreeNode> stack = new Stack<>();
+            int res = Integer.MAX_VALUE;
+            TreeNode curr = root, pre = null;
+            while (curr != null || !stack.isEmpty()) {
+                if (curr != null) {
+                    stack.push(curr);
+                    curr = curr.left;
+                } else {
+                    curr = stack.pop();
+                    if (pre != null) {
+                        res = Math.min(res, curr.val - pre.val);
+                    }
+                    pre = curr;
+                    curr = curr.right;
+                }
+            }
+            return res;
+        }
+    }
+
 }
