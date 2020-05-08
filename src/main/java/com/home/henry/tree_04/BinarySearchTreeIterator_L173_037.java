0b5270051b039b7b31e777a531f7fe87b99f4c4c
Henry <qq98982@gmail.com>
2020-05-08 22:48:57 +0800

    173. Binary Search Tree Iterator

--- src/main/java/com/home/henry/tree_04/BinarySearchTreeIterator_L173_037.java
+++ src/main/java/com/home/henry/tree_04/BinarySearchTreeIterator_L173_037.java
@@ -3,32 +3,64 @@ package com.home.henry.tree_04;
 import java.util.Stack;
 
 /**
+ * 173. Binary Search Tree Iterator
+ *
+ * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+ *
+ * Calling next() will return the next smallest number in the BST.
+ *
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // return 3
+ * iterator.next();    // return 7
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 9
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 15
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 20
+ * iterator.hasNext(); // return false
+ *
+ *
+ * Note:
+ * next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+ * You may assume that next() call will always be valid, that is, there will be at least a next smallest number in the BST when next() is called.
  * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
  * Calling next() will return the next smallest number in the BST.
+ *
+ * 这道题主要就是考二叉树的中序遍历的非递归形式，需要额外定义一个栈来辅助，二叉搜索树的建树规则就是左<根<右，用中序遍历即可从小到大取出所有节点。
  */
 public class BinarySearchTreeIterator_L173_037 {
 
-    private Stack<TreeNode> stack;
+    static class Solution {
+        public class BSTIterator {
+            private Stack<TreeNode> stack = new Stack<TreeNode>();
 
-    public BinarySearchTreeIterator_L173_037(TreeNode root) {
-        stack = new Stack<>();
-        TreeNode cur = root;
-        while (cur != null) {
-            stack.push(cur);
-            if (cur.left != null) {
-                cur = cur.left;
-            } else {
-                break;
+            public BSTIterator(TreeNode root) {
+                pushAll(root);
+            }
+
+            public boolean hasNext() {
+                return !stack.isEmpty();
+            }
+
+            public int next() {
+                TreeNode tmpNode = stack.pop();
+                pushAll(tmpNode.right);
+                return tmpNode.val;
+            }
+
+            private void pushAll(TreeNode node) {
+                for (; node != null; stack.push(node), node = node.left) {}
             }
         }
     }
 
-    /** @return the next smallest number */
-    public int next() {
-        TreeNode node = stack.pop();
-        TreeNode cur = node;
-        if (cur.right != null) {
-            cur = cur.right;
+    static class SolutionSec {
+        private Stack<TreeNode> stack;
+
+        public SolutionSec(TreeNode root) {
+            stack = new Stack<>();
+            TreeNode cur = root;
             while (cur != null) {
                 stack.push(cur);
                 if (cur.left != null) {
@@ -38,37 +70,28 @@ public class BinarySearchTreeIterator_L173_037 {
                 }
             }
         }
-        return node.val;
-    }
 
-    /** @return whether we have a next smallest number */
-    public boolean hasNext() {
-        return !stack.isEmpty();
-    }
-    /**
-     * 还有第二种方法更容易理解一些  O(h) memory, hasNext() in O(1) time, next() is O(h) time.
-     */
-//
-//    private Stack<TreeNode> stack = new Stack<TreeNode>();
-//
-//    public BSTIterator(TreeNode root) {
-//        pushAll(root);
-//    }
-//
-//    /** @return whether we have a next smallest number */
-//    public boolean hasNext() {
-//        return !stack.isEmpty();
-//    }
-//
-//    /** @return the next smallest number */
-//    public int next() {
-//        TreeNode tmpNode = stack.pop();
-//        pushAll(tmpNode.right);
-//        return tmpNode.val;
-//    }
-//
-//    private void pushAll(TreeNode node) {
-//        for (; node != null; stack.push(node), node = node.left);
-//    }
+        /** @return the next smallest number */
+        public int next() {
+            TreeNode node = stack.pop();
+            TreeNode cur = node;
+            if (cur.right != null) {
+                cur = cur.right;
+                while (cur != null) {
+                    stack.push(cur);
+                    if (cur.left != null) {
+                        cur = cur.left;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return node.val;
+        }
 
+        /** @return whether we have a next smallest number */
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    }
 }
