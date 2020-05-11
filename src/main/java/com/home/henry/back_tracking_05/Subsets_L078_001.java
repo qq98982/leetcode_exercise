06b3944a6620af4491a597a46d08ee6a6685d2fe
Henry <qq98982@gmail.com>
2020-05-11 10:36:11 +0800

    78. Subsets

--- src/main/java/com/home/henry/back_tracking_05/Subsets_L078_001.java
+++ src/main/java/com/home/henry/back_tracking_05/Subsets_L078_001.java
@@ -34,4 +34,34 @@ public class Subsets_L078_001 {
             }
         }
     }
+
+    /**
+     * While iterating through all numbers, for each new number, we can either pick it or not pick it
+     * 1, if pick, just add current number to every existing subset.
+     * 2, if not pick, just leave all existing subsets as they are.
+     * We just combine both into our result.
+     *
+     * For example, {1,2,3} intially we have an emtpy set as result [ [ ] ]
+     * Considering 1, if not use it, still [ ], if use 1, add it to [ ], so we have [1] now
+     * Combine them, now we have [ [ ], [1] ] as all possible subset
+     *
+     * Next considering 2, if not use it, we still have [ [ ], [1] ], if use 2, just add 2 to each previous subset, we have [2], [1,2]
+     * Combine them, now we have [ [ ], [1], [2], [1,2] ]
+     *
+     * Next considering 3, if not use it, we still have [ [ ], [1], [2], [1,2] ], if use 3, just add 3 to each previous subset, we have [ [3], [1,3], [2,3], [1,2,3] ]
+     * Combine them, now we have [ [ ], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3] ]
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int n : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(res.get(i));
+                subset.add(n);
+                res.add(subset);
+            }
+        }
+        return res;
+    }
 }
