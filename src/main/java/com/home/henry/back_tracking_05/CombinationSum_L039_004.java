1f82cbc8fa951eb46624b724c7e66e818c473e80
Henry <qq98982@gmail.com>
2020-05-11 18:35:29 +0800

    39. Combination Sum

--- src/main/java/com/home/henry/back_tracking_05/CombinationSum_L039_004.java
+++ src/main/java/com/home/henry/back_tracking_05/CombinationSum_L039_004.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 /**
  * 39. Combination Sum
+ *
  * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
  * The same repeated number may be chosen from candidates unlimited number of times.
  * Note: All numbers (including target) will be positive integers.
@@ -16,28 +17,31 @@ import java.util.List;
  * [ [2,2,2,2], [2,3,3], [3,5] ]
  *
  * NOTE: not i + 1 because we can reuse same elements !!!
+ *
+ * 就是先向前列举所有情况，得到一个解或者走不通的时候就回溯。和37题有异曲同工之处，也算是回溯法很典型的应用
+ * dp解法可以参考 https://leetcode.wang/leetCode-39-Combination-Sum.html#%E9%A2%98%E7%9B%AE%E6%8F%8F%E8%BF%B0%EF%BC%88%E4%B8%AD%E7%AD%89%E9%9A%BE%E5%BA%A6%EF%BC%89
  */
 public class CombinationSum_L039_004 {
 
     static class Solution {
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
             List<List<Integer>> res = new ArrayList<>();
-            helper(res, new ArrayList<>(), candidates, target, 0);
+            helper(res, new ArrayList<Integer>(), candidates, target, 0);
             return res;
         }
 
-        private void helper(List<List<Integer>> res, ArrayList<Integer> list, int[] candidates, int target,
-                            int index) {
-            if (target < 0) {
+        private void helper(List<List<Integer>> res, List<Integer> list, int[] candi, int remain, int start) {
+            if (remain < 0) {
                 return;
-            } else if (target == 0){
-                res.add(new ArrayList<>(list));
-            } else {
-                for (int i = index; i < candidates.length; i++ ) {
-                    list.add(candidates[i]);
-                    helper(res, list, candidates, target - candidates[i], i);
-                    list.remove(list.size() - 1);
-                }
+            } else if (remain == 0) {
+                res.add(new ArrayList<Integer>(list));
+                return;
+            }
+            for (int i = start; i < candi.length; i++) {
+                list.add(candi[i]);
+                helper(res, list, candi, remain - candi[i], i);
+                //找到了一个解或者 remain < 0 了，将当前数字移除，然后继续尝试
+                list.remove(list.size() - 1);
             }
         }
     }
