2e5b4a937638ba29b2d5140688fa95cee57af28d
Henry <qq98982@gmail.com>
2020-05-13 19:10:48 +0800

    31. Next Permutation

--- src/main/java/com/home/henry/back_tracking_05/NextPermutation_L031_009.java
+++ src/main/java/com/home/henry/back_tracking_05/NextPermutation_L031_009.java
@@ -4,6 +4,16 @@ import java.util.Arrays;
 
 /**
  * 31. Next Permutation
+ *
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place and use only constant extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ *
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ *
  * 这个主要是从后往前找出第一个first small, firstSmall = nums[i] < nums[i + 1]
  * 然后找出第一个比这个firstSmall大的数字
  * 交换这两个数字后, 将这个small后的数字全部反转(如果没有firstSmall, 那就是全部反转)
@@ -11,31 +21,44 @@ import java.util.Arrays;
  */
 public class NextPermutation_L031_009 {
 
+    // 1 2 7 4 3 1
+    // firstSmall = 1 nums[firstSmall]= 2
+    // firstBig = 5 nums[firstBig] = 3
+    // 1 3 7 4 2 1
+    // 1 3 (7 4 2 1)
+    // 1 3 1 2 4 7
     static class Solution {
         public void nextPermutation(int[] nums) {
             if (nums == null || nums.length == 0) {return;}
+            //找到第一个不再递增的位置
             int firstSmall = -1;
             for (int i = nums.length - 2; i >= 0; i--) {
                 if (nums[i] < nums[i + 1]) {
                     firstSmall = i;
+                    // 找到了要break
                     break;
                 }
             }
+            // 如果数组本来就是倒序的
             if (firstSmall == -1) {
                 reverse(nums, 0, nums.length - 1);
+                // 记得return
                 return;
             }
 
+            //找到刚好大于 nums[firstSmall]的位置
             int firstLarge = -1;
             for (int i = nums.length - 1; i >= 0; i--) {
                 if (nums[i] > nums[firstSmall]) {
                     firstLarge = i;
+                    // 找到了要break
                     break;
                 }
             }
+            // 1 2 7 4 3 1 -> 1 3 7 4 2 1
             swap(nums, firstSmall, firstLarge);
+            // 需要reverse 1 3 (7 4 2 1)
             reverse(nums, firstSmall + 1, nums.length - 1);
-            return;
         }
 
         private void swap(int[] nums, int start, int end) {
