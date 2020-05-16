5cb75941ee7063492accc297f901c76551c3593b
Henry <qq98982@gmail.com>
2020-05-16 17:30:47 +0800

    282. Expression Add Operators

--- src/main/java/com/home/henry/others/ExpressionAddOperators_L282_030.java
+++ src/main/java/com/home/henry/others/ExpressionAddOperators_L282_030.java
@@ -5,51 +5,79 @@ import java.util.List;
 
 /**
  * 282. Expression Add Operators
- * 典型的back tracking问题, 三个版本, 后两个分别是用StringBuilder和char array优化
- * 后两个保留用StringBuilder要用setLength()方法, 否则leetcode不通过
+ *
+ * Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary
+ * operators (not unary) +, -, or * between the digits so they evaluate to the target value.
+ *
+ * Input: num = "123", target = 6 Output: ["1+2+3", "1*2*3"]
+ *
+ * Input: num = "232", target = 8 Output: ["2*3+2", "2+3*2"]
+ *
+ * Input: num = "105", target = 5 Output: ["1*0+5","10-5"]
+ *
+ * Input: num = "00", target = 0 Output: ["0+0", "0-0", "0*0"]
+ *
+ * Input: num = "3456237490", target = 9191 Output: []
+ *
+ * 典型的back tracking问题, 后一个用StringBuilder和char array优化
+ * 用StringBuilder要用setLength()方法截取长度, 否则leetcode不能通过
  */
 public class ExpressionAddOperators_L282_030 {
 
     static class SolutionOri {
         List<String> addOperators(String num, int target) {
             List<String> res = new ArrayList<>();
-
             helper(res, "", num, target, 0, 0, 0);
             return res;
         }
 
-        private void helper(List<String> res, String path, String str, int target, int pos, long val,
+        List<String> addOp(String num, int target) {
+            List<String> res = new ArrayList<>();
+            helper2(res, "", num, target, 0, 0, 0);
+            return res;
+        }
+
+        private void helper2(List<String> res, String path, String num, int target, int pos, long val,
+                             long pre) {
+            if (pos == num.length() && target == val) {
+                res.add(path);
+                return;
+            }
+        }
+
+        private void helper(List<String> res, String path, String num, int target, int pos, long val,
                             long pre) {
-            if (pos == str.length()) {
+            if (pos == num.length()) {
                 if (target == val) {
                     res.add(path);
                     return;
                 }
             }
-            for (int i = pos; i < str.length(); i++) {
-                if (i != pos && str.charAt(pos) == '0') {break;}
-                long cur = Long.parseLong(str.substring(pos, i + 1));
+            for (int i = pos; i < num.length(); i++) {
+                if (i != pos && num.charAt(pos) == '0') {break;}
+                long cur = Long.parseLong(num.substring(pos, i + 1));
+                // 第一个数不加符号
                 if (pos == 0) {
-                    helper(res, path + cur, str, target, i + 1, cur, cur);
+                    helper(res, path + cur, num, target, i + 1, cur, cur);
                 } else {
-                    helper(res, path + "+" + cur, str, target, i + 1, val + cur, cur);
-                    helper(res, path + "-" + cur, str, target, i + 1, val - cur, -cur);
-                    helper(res, path + "*" + cur, str, target, i + 1, val - pre + pre * cur, pre * cur);
+                    helper(res, path + "+" + cur, num, target, i + 1, val + cur, cur);
+                    helper(res, path + "-" + cur, num, target, i + 1, val - cur, -cur);
+                    helper(res, path + "*" + cur, num, target, i + 1, val - pre + pre * cur, pre * cur);
                 }
             }
         }
     }
 
     static class Solution {
-        List<String> addOperators(String num, int target) {
+        public static List<String> addOperators(String num, int target) {
             List<String> res = new ArrayList<>();
             StringBuilder path = new StringBuilder();
             helper(res, path, num.toCharArray(), target, 0, 0, 0);
             return res;
         }
 
-        void helper(List<String> res, StringBuilder path, char[] ch, int target, int pos, long val,
-                    long pre) {
+        static void helper(List<String> res, StringBuilder path, char[] ch, int target, int pos, long val,
+                           long pre) {
             if (pos == ch.length && target == val) {
                 res.add(path.toString());
                 return;
@@ -60,6 +88,7 @@ public class ExpressionAddOperators_L282_030 {
                 if (i != pos && ch[pos] == '0') {break;}
                 cur = cur * 10 + ch[i] - '0';
                 int len = path.length();
+                // 第一个数不加符号
                 if (pos == 0) {
                     helper(res, path.append(cur), ch, target, i + 1, cur, cur);
                     // 重置为最初的长度
@@ -79,10 +108,9 @@ public class ExpressionAddOperators_L282_030 {
     }
 
     public static void main(String[] args) {
-        SumToTarget_FB.Solution s = new SumToTarget_FB.Solution();
-        System.out.println(s.sumToTarget("1234567890", 100));
-        System.out.println(s.sumToTarget("1230", 6));
-        System.out.println(s.sumToTarget("00", 0));
-        System.out.println(s.sumToTarget("34", 1));
+        System.out.println(Solution.addOperators("1234567890", 100));
+        System.out.println(Solution.addOperators("1230", 6));
+        System.out.println(Solution.addOperators("00", 0));
+        System.out.println(Solution.addOperators("34", 1));
     }
 }
