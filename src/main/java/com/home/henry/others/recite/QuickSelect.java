097461e586e7c0fcfaab791b04cd2609eb914448
Henry <qq98982@gmail.com>
2020-05-26 18:31:40 +0800

    Quick Select

--- src/main/java/com/home/henry/others/recite/QuickSelect.java
+++ src/main/java/com/home/henry/others/recite/QuickSelect.java
@@ -2,49 +2,56 @@ package com.home.henry.others.recite;
 
 import java.util.Arrays;
 
+/**
+ * Quick Select
+ *
+ * nums contains duplicate numbers
+ */
 public class QuickSelect {
-    public static void quick(int[] arr, int start, int end, int k) {
-        if (start >= end) {return;}
-        int partition = partition(arr, start, end);
-        if (k - 1 < partition) {
-            quick(arr, start, partition - 1, k);
-        } else {
-            quick(arr, partition + 1, end, k);
-        }
-    }
-
-    private static int partition(int[] arr, int start, int end) {
-        int num = arr[start];
-        int i = start, j = end + 1;
-        while (true) {
-            while (arr[++i] <= num) {
-                if (i == end) { break;}
+    static class Solution {
+        public static int select(int[] nums, int k) {
+            int start = 0, end = nums.length - 1;
+            while (true) {
+                int p = partition(nums, start, end);
+                if (p + 1 == k) {
+                    return nums[p];
+                } else if (p + 1 > k) {
+                    end = p - 1;
+                } else {
+                    start = p + 1;
+                }
             }
-            while (arr[--j] >= num) {
-                if (j == start) { break;}
-            }
-            if (i >= j) { break;}
-            exch(arr, i, j);
         }
-        exch(arr, start, j);
-        return j;
-    }
 
-    private static void exch(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+        private static int partition(int[] nums, int start, int end) {
+            if (start == end) {return start;}
+            int num = nums[start];
+            int i = start, j = end + 1;
+            while (true) {
+                while (nums[++i] <= num) {
+                    if (i == end) {break;}
+                }
+                while (nums[--j] >= num) {
+                    if (j == start) {break;}
+                }
+                if (i >= j) {break;}
+                exch(nums, i, j);
+            }
+            exch(nums, start, j);
+            return j;
+        }
 
-    public static int select(int[] arr, int k) {
-        quick(arr, 0, arr.length - 1, k);
-        return arr[k - 1];
+        private static void exch(int[] nums, int i, int j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = { 10, 9, 11, 12, 15, 8, 2, 7 };
         System.out.println(Arrays.toString(arr));
-        System.out.println(select(arr, 6));
+        System.out.println(Solution.select(arr, 6));
         System.out.println(Arrays.toString(arr));
     }
 }
