c38b4050c0e7bba83ab6dcfe8b57de83202d5353
Henry <qq98982@gmail.com>
2020-05-26 10:23:05 +0800

    232. Implement Queue using Stacks

--- src/main/java/com/home/henry/stack_pq_11/ImplementQueueUsingStacks_L232_002.java
+++ src/main/java/com/home/henry/stack_pq_11/ImplementQueueUsingStacks_L232_002.java
@@ -4,35 +4,61 @@ import java.util.Stack;
 
 /**
  * 232. Implement Queue using Stacks
+ *
+ * Implement the following operations of a queue using stacks.
+ *
+ * push(x) -- Push element x to the back of queue.
+ * pop() -- Removes the element from in front of queue.
+ * peek() -- Get the front element.
+ * empty() -- Return whether the queue is empty.
+ * Example:
+ *
+ * MyQueue queue = new MyQueue();
+ *
+ * queue.push(1);
+ * queue.push(2);
+ * queue.peek();  // returns 1
+ * queue.pop();   // returns 1
+ * queue.empty(); // returns false
  */
 public class ImplementQueueUsingStacks_L232_002 {
 
-    private Stack<Integer> queue;
+    static class Solution {
+        private Stack<Integer> queue;
 
-    public ImplementQueueUsingStacks_L232_002() {
-        this.queue = new Stack<>();
-    }
-
-    public void push(int x) {
-        Stack<Integer> temp = new Stack<>();
-        while (!queue.isEmpty()) {
-            temp.push(queue.pop());
+        Solution() {
+            queue = new Stack<>();
         }
-        queue.push(x);
-        while (!temp.isEmpty()) {
-            queue.push(temp.pop());
+
+        public void push(int x) {
+            Stack<Integer> tmp = new Stack<>();
+            while (queue.isEmpty()) {
+                tmp.push(queue.pop());
+            }
+            queue.push(x);
+            while (!tmp.isEmpty()) {
+                queue.push(tmp.pop());
+            }
         }
-    }
 
-    public int pop() {
-        return queue.pop();
-    }
+        public int pop() {
+            if (!queue.isEmpty()) {
+                return queue.pop();
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
 
-    public int peek() {
-        return queue.peek();
-    }
+        public int peek() {
+            if (!queue.isEmpty()) {
+                return queue.peek();
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
 
-    public boolean isEmpty() {
-        return queue.isEmpty();
+        public boolean isEmpty() {
+            return queue.isEmpty();
+        }
     }
 }
