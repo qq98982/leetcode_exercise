5740855a04bc43cea3dd7f8f7e1038ad28245bc3
Henry <qq98982@gmail.com>
2020-05-27 10:55:13 +0800

    171. Excel Sheet Column Number

--- src/main/java/com/home/henry/string_02/ExcelSheetColumnNumber_L171_017.java
+++ src/main/java/com/home/henry/string_02/ExcelSheetColumnNumber_L171_017.java
@@ -1,7 +1,22 @@
 package com.home.henry.string_02;
 
 /**
+ * 171. Excel Sheet Column Number
+ *
  * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ *
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ *
+ * Input: "A" Output: 1
+ * Input: "AB" Output: 28
+ * Input: "ZY" Output: 701
  */
 public class ExcelSheetColumnNumber_L171_017 {
 
