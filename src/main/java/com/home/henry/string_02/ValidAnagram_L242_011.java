2bd350a56e9453a9bd9b97974049ca11f312349d
Henry <qq98982@gmail.com>
2020-05-27 10:35:39 +0800

    242. Valid Anagram

--- src/main/java/com/home/henry/string_02/ValidAnagram_L242_011.java
+++ src/main/java/com/home/henry/string_02/ValidAnagram_L242_011.java
@@ -5,8 +5,18 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ *
+ * 242. Valid Anagram
+ *
  * Given two strings s and t , write a function to determine if t is an anagram of s.
  * Input: s = "anagram", t = "nagaram" Output: true
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your solution to such case?
  */
 public class ValidAnagram_L242_011 {
 
@@ -28,5 +38,24 @@ public class ValidAnagram_L242_011 {
         }
         Map<int[], List<String>> map = new HashMap<>();
         return true;
+    }
+
+    static class Solution {
+        public boolean isAnagram(String s, String t) {
+            int[] bucket = new int[26];
+            char[] sch = s.toCharArray();
+            char[] tch = t.toCharArray();
+            if (sch.length != tch.length) {return false;}
+            for (char c : sch) {
+                bucket[c - 'a']++;
+            }
+            for (char c : tch) {
+                bucket[c - 'a']--;
+            }
+            for (int n : bucket) {
+                if (n != 0) {return false;}
+            }
+            return true;
+        }
     }
 }
