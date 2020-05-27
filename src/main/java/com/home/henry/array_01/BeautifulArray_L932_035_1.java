9019684fa1d466a11e17be8a2f15257c1b2d90b5
Henry <qq98982@gmail.com>
2020-05-27 18:53:03 +0800

    932. Beautiful Array

--- src/main/java/com/home/henry/array_01/BeautifulArray_L932_035_1.java
+++ src/main/java/com/home/henry/array_01/BeautifulArray_L932_035_1.java
@@ -7,15 +7,14 @@ import java.util.List;
 /**
  * 932. Beautiful Array
  *
- * For some fixed N, an array A is beautiful if it is a permutation of the integers 1, 2, ..., N, such that:
- *
+ * For some fixed N, an array A is beautiful if it is a permutation of the integers 1, 2, ..., N,
+ * such that:
  * For every i < j, there is no k with i < k < j such that A[k] * 2 = A[i] + A[j].
- *
  * Given N, return any beautiful array A.  (It is guaranteed that one exists.)
- *
  * Input: 4 Output: [2,1,4,3]
  * Input: 5 Output: [3,1,2,5,4]
- * 这个题目的意思比较奇怪, 用迭代的方式, 求出结果
+ * 观察左边都是奇数,右边都是偶数是符合这个定义的.在这个基础上,每个数字*2是符合要求的,然后相加也是符合要求的.
+ * 用迭代的方式, 求出结果
  */
 public class BeautifulArray_L932_035_1 {
 
