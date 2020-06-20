c7dc74db67c388a84b590b091068c556bb2e5765
Henry <qq98982@gmail.com>
2020-06-20 10:32:02 +0800

    241. Different Ways to Add Parentheses

--- src/main/java/com/home/henry/string_02/DifferentWaysToAddParentheses_L241_029.java
+++ src/main/java/com/home/henry/string_02/DifferentWaysToAddParentheses_L241_029.java
@@ -35,15 +35,17 @@ public class DifferentWaysToAddParentheses_L241_029 {
 
         private List<Integer> helper(String input, Map<String, List<Integer>> cache) {
             if (cache.containsKey(input)) {return cache.get(input);}
-            // 运算符号
+            // 是否包含运算符号
             boolean expression = false;
             List<Integer> result = new ArrayList<>();
             for (int i = 0; i < input.length(); i++) {
+                // 这个i位置的字符如果是运算符 即不是-1, 那么分别算出左边和右边的, 再算出结果加入到result中
                 if ("+-*".indexOf(input.charAt(i)) != -1) {
                     List<Integer> left = helper(input.substring(0, i), cache);
                     List<Integer> right = helper(input.substring(i + 1), cache);
                     for (int l : left) {
                         for (int r : right) {
+                            // 计算每一个可能的值, 加入到result中
                             int res = cal(l, r, input.charAt(i));
                             result.add(res);
                         }
@@ -51,16 +53,17 @@ public class DifferentWaysToAddParentheses_L241_029 {
                     expression = true;
                 }
             }
-            // 非运算符号才加入result
+            // 如果这个字符串整体不包含运算符, 只有数字, 因为前面没有包含这个判断, 所以也要额外加入到result中
             if (!expression) {
                 result.add(Integer.parseInt(input));
             }
+            // 放入cache中
             cache.put(input, result);
             return result;
         }
 
         private static Integer cal(Integer l, Integer r, Character c) {
-            int res = 0;
+            int res;
             switch (c) {
                 case '+':
                     res = l + r;
