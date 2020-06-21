4b2be643fd0c130a8f76c9252ecb8b1a5cf890e1
Henry <qq98982@gmail.com>
2020-06-21 16:50:11 +0800

    56. Merge Intervals

--- src/main/java/com/home/henry/array_01/MergeIntervals_L056_046.java
+++ src/main/java/com/home/henry/array_01/MergeIntervals_L056_046.java
@@ -11,6 +11,7 @@ import java.util.List;
  * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
  * Input: [[1,4],[4,5]] Output: [[1,5]]
  * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ *
  * 很重要, 扫描线算法, 类似题目有252 253 435
  */
 public class MergeIntervals_L056_046 {
@@ -20,18 +21,18 @@ public class MergeIntervals_L056_046 {
             // init, should sort arrays
             Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
             List<int[]> res = new ArrayList<>();
-            int[] initInterval = intervals[0];
+            int[] start = intervals[0];
             // add old one
-            res.add(initInterval);
-            for (int[] curr : intervals) {
+            res.add(start);
+            for (int[] entry : intervals) {
                 // new one's first number is not big enough, should merge
-                if (curr[0] <= initInterval[1]) {
+                if (entry[0] <= start[1]) {
                     // update old one's 1 from maximum of itself and new one's second number
-                    initInterval[1] = Math.max(curr[1], initInterval[1]);
+                    start[1] = Math.max(entry[1], start[1]);
                 } else {
                     // new array's first number is bigger than old one's second number, add and update
-                    initInterval = curr;
-                    res.add(initInterval);
+                    start = entry;
+                    res.add(start);
                 }
             }
             return res.toArray(new int[res.size()][]);
