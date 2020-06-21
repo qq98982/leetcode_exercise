b7f9f64558df987b743697a4c4ff1193941d1d4e
Henry <qq98982@gmail.com>
2020-06-21 13:10:22 +0800

    14. Longest Common Prefix

--- src/main/java/com/home/henry/string_02/LongestCommonPrefix_L014_002.java
+++ src/main/java/com/home/henry/string_02/LongestCommonPrefix_L014_002.java
@@ -4,7 +4,14 @@ package com.home.henry.string_02;
  * 14. Longest Common Prefix
  *
  * Write a function to find the longest common prefix string amongst an array of strings.
+ *
  * If there is no common prefix, return an empty string "".
+ *
+ * Input: ["flower","flow","flight"] Output: "fl"
+ * Input: ["dog","racecar","car"] Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ * Note: All given inputs are in lowercase letters a-z.
  */
 public class LongestCommonPrefix_L014_002 {
 
