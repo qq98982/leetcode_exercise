1f74c6a15919f3d6f798e0a5d1805ed3daa420df
Henry <qq98982@gmail.com>
2020-06-21 09:00:47 +0800

    79. Word Search

--- src/main/java/com/home/henry/dfs_10_1/WordSearch_L079_011.java
+++ src/main/java/com/home/henry/dfs_10_1/WordSearch_L079_011.java
@@ -5,28 +5,19 @@ package com.home.henry.dfs_10_1;
  *
  * Given a 2D board and a word, find if the word exists in the grid.
  *
- * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells
+ * are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
  *
- * Example:
- *
- * board =
- * [
+ * board = [
  *   ['A','B','C','E'],
  *   ['S','F','C','S'],
  *   ['A','D','E','E']
  * ]
- *
  * Given word = "ABCCED", return true.
  * Given word = "SEE", return true.
  * Given word = "ABCB", return false.
  *
- *
- * Constraints:
- *
  * board and word consists only of lowercase and uppercase English letters.
- * 1 <= board.length <= 200
- * 1 <= board[i].length <= 200
- * 1 <= word.length <= 10^3
  */
 public class WordSearch_L079_011 {
 
@@ -46,11 +37,14 @@ public class WordSearch_L079_011 {
         }
 
         private boolean dfs(char[][] board, String word, int row, int col, int n) {
+            // 退出条件
             if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {return false;}
 
+            // 第一个字母就不符合, 退出
             if (word.charAt(n) != board[row][col]) {
                 return false;
             }
+            // 走到单词最后一个字母, 完成
             if (n == word.length() - 1) {
                 return true;
             }
