0a779d122664d6f87c37db61388ba2f366285ea1
Henry <hongyu_98982@163.com>
2020-10-04 15:03:58 +0800

    449. Serialize and Deserialize BST

--- src/main/java/com/home/henry/tree_04/L449_SerializeAndDeserializeBST_040.java
+++ src/main/java/com/home/henry/tree_04/L449_SerializeAndDeserializeBST_040.java
@@ -1,33 +1,67 @@
 package com.home.henry.tree_04;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 449. Serialize and Deserialize BST
- * because we need to update idx along with the recursion. If we pass int, then it's just a local variable for
- * current recursion. What we need is an object. And one more thing, why not Integer? Because Integer is
- * immutable like String. Passing in Integer is actually same as int.
+ *
+ * Serialization is converting a data structure or object into a sequence of bits so that it
+ * can be stored in a file or memory buffer, or transmitted across a network connection link
+ * to be reconstructed later in the same or another computer environment.
+ *
+ * Design an algorithm to serialize and deserialize a binary search tree. There is no
+ * restriction on how your serialization/deserialization algorithm should work.
+ * You need to ensure that a binary search tree can be serialized to a string, and this
+ * string can be deserialized to the original tree structure.
+ *
+ * The encoded string should be as compact as possible.
+ *
+ * Input: root = [2,1,3] Output: [2,1,3]
+ * Input: root = [] Output: []
+ *
+ * https://leetcode.com/problems/serialize-and-deserialize-bst/discuss/177617/the-General-Solution-for-Serialize-and-Deserialize-BST-and-Serialize-and-Deserialize-BT
  */
 public class L449_SerializeAndDeserializeBST_040 {
 
-    public String serialize(TreeNode root) {
-        if (root == null) {return "#";}
-        return String.valueOf(root.val) + "," + serialize(root.left) + "," + serialize(root.right);
-    }
+    /**
+     * use upper and lower boundaries to check whether we should add null
+     */
+    public class Codec {
 
-    public TreeNode deserialize(String data) {
-        String[] nodeString = data.split(",");
-
-        return deserialize(nodeString, new int[]{0});
-    }
-
-    private TreeNode deserialize(String[] arr, int[] idx) {
-        if(arr[idx[0]].equals("#")) {
-            idx[0]++;
-            return null;
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+            StringBuilder sb = new StringBuilder();
+            serialize(root, sb);
+            return sb.toString();
         }
-        TreeNode root = new TreeNode(Integer.parseInt(arr[idx[0]++]));
-        root.left = deserialize(arr, idx);
-        root.right = deserialize(arr, idx);
-        return root;
+
+        public void serialize(TreeNode root, StringBuilder sb) {
+            if (root == null) { return; }
+            sb.append(root.val).append(",");
+            serialize(root.left, sb);
+            serialize(root.right, sb);
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            if (data.isEmpty()) { return null; }
+            Queue<String> q = new LinkedList<>(Arrays.asList(data.split(",")));
+            return deserialize(q, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+
+        public TreeNode deserialize(Queue<String> q, int lower, int upper) {
+            if (q.isEmpty()) { return null; }
+            String s = q.peek();
+            int val = Integer.parseInt(s);
+            if (val < lower || val > upper) { return null; }
+            q.poll();
+            TreeNode root = new TreeNode(val);
+            root.left = deserialize(q, lower, val);
+            root.right = deserialize(q, val, upper);
+            return root;
+        }
     }
 
 }
