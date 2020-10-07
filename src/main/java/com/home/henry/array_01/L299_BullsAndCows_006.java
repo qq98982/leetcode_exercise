e9493213435f63f4e7346082bf23b704f5c3fd67
Henry <hongyu_98982@163.com>
2020-10-07 17:49:15 +0800

    299. Bulls and Cows

--- src/main/java/com/home/henry/array_01/L299_BullsAndCows_006.java
+++ src/main/java/com/home/henry/array_01/L299_BullsAndCows_006.java
@@ -2,8 +2,16 @@ package com.home.henry.array_01;
 
 /**
  * 299. Bulls and Cows
- * You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
- * Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows.
+ *
+ * You are playing the following Bulls and Cows game with your friend: You write down a number
+ * and ask your friend to guess what the number is. Each time your friend makes a guess,
+ * you provide a hint that indicates how many digits in said guess match your secret number
+ * exactly in both digit and position (called "bulls") and how many digits match the secret
+ * number but locate in the wrong position (called "cows"). Your friend will use successive
+ * guesses and hints to eventually derive the secret number.
+ * Write a function to return a hint according to the secret number and friend's guess,
+ * use A to indicate the bulls and B to indicate the cows.
+ *
  * Please note that both secret number and friend's guess may contain duplicate digits.
  * Input: secret = "1807", guess = "7810"  Output: "1A3B"
  * Input: secret = "1123", guess = "0111"  Output: "1A1B"
@@ -32,7 +40,7 @@ public class L299_BullsAndCows_006 {
             }
         }
         // 注意这里的diff要除2
-        int bNum = 4 - diff / 2 - aNum;
+        int bNum = s.length - diff / 2 - aNum;
         return aNum + "A" + bNum + "B";
     }
 
