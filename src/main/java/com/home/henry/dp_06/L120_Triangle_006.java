a06aa546ee77175b6e403cbbcb06e9cbf2a74b6d
朱虹宇 <hongyu_98982@163.com>
2020-11-24 00:06:26 +0800

    118. Pascal's Triangle

--- src/main/java/com/home/henry/dp_06/L120_Triangle_006.java
+++ src/main/java/com/home/henry/dp_06/L120_Triangle_006.java
@@ -3,7 +3,23 @@ package com.home.henry.dp_06;
 import java.util.List;
 
 /**
- * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+ * 120. Triangle
+ *
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers
+ * on the row below.
+ * For example, given the following triangle
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ *
+ * dp[i][j] 表示从点 (i, j)(i,j) 到底边的最小路径和。
+ * dp[i][j]=min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j]
+ * 因为计算 dp[i][j]dp[i][j] 时，只用到了下一行的 dp[i + 1][j]dp[i+1][j] 和 dp[i + 1][j + 1]dp[i+1][j+1]。
+ * 因此 dp 数组不需要定义 N 行，只要定义 1 行就可以
  */
 public class L120_Triangle_006 {
 
