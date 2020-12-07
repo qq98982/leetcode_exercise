7361d0d3685d660a7608f762942c506da3c9dd85
朱虹宇 <hongyu_98982@163.com>
2020-12-07 10:50:29 +0800

    207. Course Schedule

--- src/main/java/com/home/henry/topo_logical_13/L207_CourseSchedule_001.java
+++ src/main/java/com/home/henry/topo_logical_13/L207_CourseSchedule_001.java
@@ -7,6 +7,21 @@ import java.util.Queue;
 
 /**
  * 207. Course Schedule
+ *
+ * There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+ * Some courses may have prerequisites, for example to take course 0 you have to first take course 1,
+ * which is expressed as a pair: [0,1]
+ * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+ *
+ * Input: numCourses = 2, prerequisites = [[1,0]] Output: true
+ * Explanation: There are a total of 2 courses to take.
+ *              To take course 1 you should have finished course 0. So it is possible.
+ *
+ * Input: numCourses = 2, prerequisites = [[1,0],[0,1]] Output: false
+ * Explanation: There are a total of 2 courses to take.
+ *              To take course 1 you should have finished course 0, and to take course 0 you should
+ *              also have finished course 1. So it is impossible.
+ *
  * 返回课程能否没有依赖而能全部完成
  * GRAPH, DFS, BFS都可以, 据说BFS更好, 但是BFS是33ms, DFS是2ms
  */
@@ -67,7 +82,7 @@ public class L207_CourseSchedule_001 {
                 indegree[prerequisites[i][1]]++;
             }
             Queue<Integer> queue = new LinkedList<>();
-            for (int i=0; i < indegree.length;i++) {
+            for (int i = 0; i < indegree.length; i++) {
                 if (indegree[i] == 0) {
                     queue.offer(i);
                 }
