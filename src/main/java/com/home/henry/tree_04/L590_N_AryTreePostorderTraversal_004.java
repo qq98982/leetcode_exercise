300745b270ef2b4edc07c3207afd72a3ae858429
zhuhongyu <zhuhongyu@sinosoft.com.cn>
2022-08-11 17:08:13 +0800

    590. N-ary Tree Postorder Traversal

--- src/main/java/com/home/henry/tree_04/L590_N_AryTreePostorderTraversal_004.java
+++ src/main/java/com/home/henry/tree_04/L590_N_AryTreePostorderTraversal_004.java
@@ -1,11 +1,13 @@
 package com.home.henry.tree_04;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 590. N-ary Tree Postorder Traversal
- *
+ * <p>
  * Given an n-ary tree, return the postorder traversal of its nodes' values.
  */
 public class L590_N_AryTreePostorderTraversal_004 {
@@ -23,5 +25,20 @@ public class L590_N_AryTreePostorderTraversal_004 {
             helper(res, node);
         }
         res.add(root.val);
+    }
+
+    public List<Integer> postorderIter(Node root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {return res;}
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node curr = stack.pop();
+            res.add(0, curr.val);
+            for (int i = 0; i < curr.children.size(); i++) {
+                stack.push(curr.children.get(i));
+            }
+        }
+        return res;
     }
 }
