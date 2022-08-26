18c67280d5101b44c0af65248c5082ea44a84883
zhuhongyu <zhuhongyu@sinosoft.com.cn>
2022-08-26 17:29:58 +0800

    965. Univalued Binary Tree

--- src/main/java/com/home/henry/tree_04/L965_UnivaluedBinaryTree_016.java
+++ src/main/java/com/home/henry/tree_04/L965_UnivaluedBinaryTree_016.java
@@ -2,13 +2,13 @@ package com.home.henry.tree_04;
 
 /**
  * 965. Univalued Binary Tree
- *
+ * <p>
  * A binary tree is univalued if every node in the tree has the same value.
  * Return true if and only if the given tree is univalued.
- *
+ * <p>
  * Input: [1,1,1,1,1,null,1]
  * Output: true
- *
+ * <p>
  * Input: [2,2,2,5,2]
  * Output: false
  */
@@ -16,8 +16,8 @@ public class L965_UnivaluedBinaryTree_016 {
 
     public boolean isUnivalTree(TreeNode root) {
         if (root == null) {return true;}
-        return (root.left == null || root.left.val == root.val && isUnivalTree(root.left)) && (
-                root.right == null || root.right.val == root.val && isUnivalTree(root.right));
+        return (root.left == null || (root.left.val == root.val && isUnivalTree(root.left))) && (
+                root.right == null || (root.right.val == root.val && isUnivalTree(root.right)));
     }
 
 }
