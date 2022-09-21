94a13a6615f40fd05cf4f60394e1aaa22d0d0db8
Henry <hongyu_98982@163.com>
2022-09-21 20:08:33 +0800

    704. Binary Search

--- src/main/java/com/home/henry/binary_search_08/L704_BinarySearch_003.java
+++ src/main/java/com/home/henry/binary_search_08/L704_BinarySearch_003.java
@@ -3,8 +3,66 @@ package com.home.henry.binary_search_08;
 /**
  * 704. Binary Search
  * 使用迭代的第二种写法
+ * https://labuladong.github.io/algo/2/20/29/
  */
 public class L704_BinarySearch_003 {
+
+    // 推荐
+    int binary_search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if(nums[mid] == target) {
+                // 直接返回
+                return mid;
+            }
+        }
+        // 直接返回
+        return -1;
+    }
+
+    int left_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 别返回，锁定左侧边界
+                right = mid - 1;
+            }
+        }
+        // 判断 target 是否存在于 nums 中
+        // 此时 target 比所有数都大，返回 -1
+        if (left == nums.length) {return -1;}
+        // 判断一下 nums[left] 是不是 target
+        return nums[left] == target ? left : -1;
+    }
+
+    int right_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 别返回，锁定右侧边界
+                left = mid + 1;
+            }
+        }
+        // 此时 left - 1 索引越界
+        if (left - 1 < 0) {return -1;}
+        // 判断一下 nums[left] 是不是 target
+        return nums[left - 1] == target ? (left - 1) : -1;
+    }
 
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {return -1;}
